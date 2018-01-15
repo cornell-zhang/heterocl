195bc5adb498c2155dadecb2b609e7847479301c
seanlatias <seanlatias@gmail.com>
2018-01-15 11:40:40 -0500

    add digitrec sample with proposed API

--- hcl/samples/digitrec.py
+++ hcl/samples/digitrec.py
@@ -1,65 +1,53 @@
-import tvm, numpy
-from hcl.parser import mybuild
+import hcl
 
-SIZE = 100
-K = 3
-
-dtype = "float32"
-
-def popcount(i):
-  '''
-  m1  = 0x5555555555555555
-  m2  = 0x3333333333333333
-  m4  = 0x0f0f0f0f0f0f0f0f
-  h01 = 0x0101010101010101
-  x -= (x >> 1) & m1
-  x = (x & m2) + ((x >> 2) & m2)
-  x = (x + (x >> 4)) & m4
-  return (x * h01) >> 56
-  '''
-  '''
-  i = i - ((i >> 1) & 0x55555555)
-  i = (i & 0x33333333) + ((i >> 2) & 0x33333333)
-  return (((i + (i >> 4)) & 0x0F0F0F0F) * 16843009) >> 24
-  '''
-  return i
-
-
-def popcount2(x, length):
+def popcount(num, nbits):
   out = 0
-  for l in range(0, length):
-    if x % 2 == 1:
+  for i in range(0, nbits):
+    if x%2 == 1:
       out = out + 1
-    x = x >> 1
+    out = out >> 1
   return out
 
-def update_knn(dist, knn_row):
-  max_id = 0
-  max_dist = 0
-  for k in range(0, 3):
-    max_id = k if knn_row[k] > max_dist else max_id
-  if dist < max_dist:
-    knn_row[max_id] = dist
+def popcount_inline(num, nbits):
+  out = 0
+  for i in range(0, nbits):
+    out = hcl.select(x%2, out+1, out)
+    out = out >> 1
+  return out
 
-def digitrec():
-
-  input_image = tvm.var("input_image")
-  labelval = tvm.placeholder((10, 100), name = 'labelval')
-  diff = tvm.compute(labelval.shape, lambda x, y: input_image ^ labelval[x][y], name = 'diff')
-  dist = tvm.compute((10, 100), lambda x, y: popcount2(diff[x][y], 49), name = 'dist')
-
-  knn_mat = tvm.placeholder((10, 3), name = 'knn_mat')
+def update_knn(dist, knn_mat):
   for i in range(0, 10):
     for j in range(0, 100):
-      update_knn(dist[i][j], knn_mat[i])
+      max_id = 0
+      for k in range(1, 3):
+        max_id = k if knn_mat[i][k] > knn_mat[i][max_id] else max_id
+      if dist[i][j] < knn_mat[i][max_id]:
+        knn_mat[i][max_id] = dist[i][j]
 
-  #s = tvm.create_schedule(knn_mat.op)
+""" function definitons and updates """
+input_image = hcl.var(name = "input_image", dtype = "int49")
+labelval = hcl.placeholder((10, 100), name = "labelval", dtype = "int49")
+diff = hcl.compute(labelval.shape, lambda x, y: input_image ^ labelval[x][y], name = "diff")
+dist = hcl.compute((10, 100), lambda x, y: popcount(diff[x][y], 49), name = "dist", inline = False)
+# alternatively, you can do this
+# dist = hcl.compute((10, 100), lambda x, y: popcount_inline(diff[x][y], 49), name = "dist")
+knn_mat = hcl.placeholder((10, 3), name = "knn_mat", dtype = "int6")
+knn_init = hcl.update(knn_mat, lambda x, y: knn_mat[x][y] = 50, name = "knn_init")
+knn_update = hcl.block(update_knn, args = [dist, knn_mat], name = "knn_update")
 
-evaluator = mybuild(digitrec, extern_func = [popcount2, update_knn], args = ["input_image", "labelval", "knn_mat"])
+""" scheduling """
+s = hcl.create_schedule(knn_update)
+s[diff].unroll(diff.axis[0]) # diff.axis = [x, y]
+s[dist].pipeline(dist.axis[1], II = 1) # dist.axis = [x, y, i]
+s[knn_init].unroll(knn_int.axis[0]) # knn_init.axis = [x, y]
+s[knn_update].pipeline(knn_update.axis[1], II = 1) # knn_update.axis = [i, j, k]
 
-'''
-data = tvm.nd.array(numpy.random.rand(10, SIZE).astype(dtype), tvm.cpu(0))
-output = tvm.nd.array(numpy.zeros((10, K), dtype = dtype), tvm.cpu(0))
+""" build the module """
+m = hcl.build(s, [input_image, label_val, knn_mat])
 
-evaluator(input_image, data, output)
-'''
+""" test the module """
+test_image = ...
+train_image = hcl.nd.array(numpy.load(...), hcl.cpu(0))
+result = hcl.nd.array(numpy.zeros((10, 3)), hcl.cpu(0))
+m(test_image, train_image, result)
+print result
