475195aa7163ade9a90c14d124d87902d676c7d9
seanlatias <seanlatias@gmail.com>
2018-03-18 16:43:08 -0400

    [HCL] Added hcl.update

--- hcl/python/hcl/api.py
+++ hcl/python/hcl/api.py
@@ -60,6 +60,28 @@ def compute(shape, inputs, fcompute, name = "compute", dtype = "float32", inline
 
   return op
 
+def update(tensor, inputs, fcompute, name = "update", extern_funcs = []):
+  code = fcompute.__code__
+  args = code.co_varnames
+
+  # collect input placeholders
+  assert (tensor not in inputs)
+  input_placeholders = [decl_buffer(i.shape, i.dtype, i.op.name) for i in inputs]
+  update_placeholder = decl_buffer(tensor.shape, tensor.dtype, tensor.op.name)
+  inputs.append(tensor)
+  input_placeholders.append(update_placeholder)
+  # infer output dtype
+  output_placeholders = [decl_buffer((1,), "int32", name)]
+  # collect body
+  if len(args) == 0: #TODO debug message
+    print "WRONG NUMBER OF ARGS!!"
+  lambda_root = visitor.LambdaVisitor().enter(inspect.getsource(code)) # extract the lambda function AST
+  body = visitor.HalideIRVisitor().compile(lambda_root, inputs, input_placeholders, update_placeholder, extern_funcs) # compile Python AST to Halide IR
+  op = _tvm_api._ExternOp(name, "", inputs, input_placeholders, output_placeholders, body)
+  op = op.output(0)
+
+  return op
+
 def block(fblock, inputs, args = [], name = "block", extern_funcs = []):
   input_placeholders = [decl_buffer(i.shape, i.dtype, i.op.name) for i in inputs]
   output_placeholders = [decl_buffer((1,), "int32", name)]
