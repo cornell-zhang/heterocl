f5f8cda695f372eadfa24b4d69972bef42792c11
seanlatias <seanlatias@gmail.com>
2018-05-15 09:40:42 -0400

    [Add] global config for initial data type
    [Fixed] API for update and block

--- heterocl/python/heterocl/api.py
+++ heterocl/python/heterocl/api.py
@@ -1,6 +1,7 @@
 from . import tensor
 from . import util
 from . import types
+from . import config
 from .code_builder import CodeBuilder
 from .resizer import Resizer, Downsizer
 from tvm.api import _IterVar, decl_buffer, convert
@@ -64,11 +65,13 @@ def convert_dtype(dtype):
     return dtype
 
 
-def var(name = "var", dtype = "int32"):
+def var(name = "var", dtype = None):
+  dtype = config.init_dtype if dtype is None else dtype
   dtype = convert_dtype(dtype)
   return tensor.Var(_var(name = name, dtype = dtype))
 
-def placeholder(shape, name = "placeholder", dtype = "int32"):
+def placeholder(shape, name = "placeholder", dtype = None):
+  dtype = config.init_dtype if dtype is None else dtype
   dtype = convert_dtype(dtype)
   builder = CodeBuilder.current
   p = tensor.Tensor(shape, dtype, name)
@@ -80,7 +83,8 @@ def placeholder(shape, name = "placeholder", dtype = "int32"):
     builder[-1].emit(lambda x: _make.Allocate(p.buf.data, dtype, shape, util.true(), x))
     return p
 
-def local(init = 0, name = "local", dtype = "int32"):
+def local(init = 0, name = "local", dtype = None):
+  dtype = config.init_dtype if dtype is None else dtype
   dtype = convert_dtype(dtype)
   builder = CodeBuilder.current
   assert len(builder) != 0, "hcl.local must be used inside a code builder"
@@ -92,13 +96,12 @@ def local(init = 0, name = "local", dtype = "int32"):
   return p
 
 # TODO: record the index of all calls and loops
-def compute(shape, inputs, fcompute, name = "compute", dtype = "int32"):
+def compute(shape, inputs, fcompute, name = "compute", dtype = None):
   code = fcompute.__code__
   args = code.co_varnames
   nargs = code.co_argcount
+  dtype = config.init_dtype if dtype is None else dtype
   dtype = convert_dtype(dtype)
-
-  #assert (len(shape) == nargs), "fcompute does not match output dimension"
 
   indices = [_IterVar((0, shape[n]), args[n], 0) for n in range(0, nargs)]
   var_list = [i.var for i in indices]
@@ -139,49 +142,62 @@ def compute(shape, inputs, fcompute, name = "compute", dtype = "int32"):
 
   return p
 
-#TODO: incorrect
-def update(tensor, inputs, fcompute, name = "update", extern = []):
-  code = fcompute.__code__
-  args = code.co_varnames
-
-  # collect input placeholders
-  input_tensors = []
-  input_vars = []
-  for i in inputs:
-    input_tensors.append(i) if isinstance(i, tvm.tensor.Tensor) else input_vars.append(i)
-  input_placeholders = [decl_buffer(i.shape, i.dtype, i.op.name) for i in input_tensors]
-  update_placeholder = decl_buffer(tensor.shape, tensor.dtype, tensor.op.name)
-  if tensor not in inputs:
-    input_tensors.append(tensor)
-    input_placeholders.append(update_placeholder)
-  # infer output dtype
-  output_placeholders = [decl_buffer((1,), "int32", name)]
-  # collect body
-  lambda_root = visitor.LambdaVisitor().enter(inspect.getsource(code)) # extract the lambda function AST
-  body = visitor.HalideIRVisitor().compile_lambda(lambda_root, input_tensors, input_placeholders, input_vars, update_placeholder, extern) # compile Python AST to Halide IR
-  op = _tvm_api._ExternOp(name, "", input_tensors, input_placeholders, output_placeholders, body)
-  op = op.output(0)
-
-  return op
-
-#TODO: incorrect
-def block(fblock, inputs, args = [], name = "block", extern = []):
-  input_placeholders = [decl_buffer(i.shape, i.dtype, i.op.name) for i in inputs]
-  output_placeholders = [decl_buffer((1,), "int32", name)]
-  # compile fblock to Halide IR
-  if len(args) == 0:
-    args = inputs
-  body = visitor.HalideIRVisitor().compile_block(fblock, inputs, input_placeholders, args, extern)
-  op = _tvm_api._ExternOp(name, "", inputs, input_placeholders, output_placeholders, body)
-  op = op.output(0)
-
-  return op
-
-def mut_compute(shape, inputs, fcompute, name = "mut_compute", dtype = "int32"):
+def update(_tensor, inputs, fcompute, name = "update"):
   code = fcompute.__code__
   args = code.co_varnames
   nargs = code.co_argcount
-  dtype = convert_dtype(dtype)
+  shape = _tensor.shape
+  dtype = _tensor.dtype
+
+  indices = [_IterVar((0, shape[n]), args[n], 0) for n in range(0, nargs)]
+  var_list = [i.var for i in indices]
+  body = None
+  p = tensor.Tensor((1,), "int32", name)
+
+  cb_count = len(CodeBuilder.stmt_stack)
+
+  ret = fcompute(*var_list)
+
+  index, _, _ = util.get_index(shape, indices, 0)
+  if isinstance(ret, tensor.TensorSlice):
+    ret = ret.asnode()
+    body = _make.Store(_tensor.buf.data, _make.Cast(dtype, ret), index)
+  elif isinstance(ret, tensor.Tensor):
+    var = _var("comp_var")
+    extent = ret.shape[0]
+    body = _make.For(var, 0, extent, 0, 0,
+        _make.Store(_tensor.buf.data, _make.Cast(dtype, ret[var]), index * extent + var))
+  elif isinstance(ret, (_expr.Expr, numbers.Number)):
+    body = _make.Store(_tensor.buf.data, _make.Cast(dtype, ret), index)
+  else:
+    raise ValueError("Unrecognized return value in hcl.compute")
+  if len(CodeBuilder.stmt_stack) == cb_count:
+    body = util.make_for(indices, body, 0)
+  else:
+    body = _make.Block(CodeBuilder.get(), body)
+    body = util.make_for(indices, body, 0)
+
+  op = tensor.Operation(inputs, p, body)
+  tensor.Operation.op_list.append(op)
+
+  return p
+
+def block(inputs, fblock, name = "block"):
+  p = tensor.Tensor((1,), "int32", name)
+
+  fblock()
+  assert len(CodeBuilder.stmt_stack) != 0
+  body = CodeBuilder.get()
+
+  op = tensor.Operation(inputs, p, body)
+  tensor.Operation.op_list.append(op)
+
+  return p
+
+def mut_compute(shape, inputs, fcompute, name = "mut_compute"):
+  code = fcompute.__code__
+  args = code.co_varnames
+  nargs = code.co_argcount
   p = tensor.Tensor((1,), "int32", name)
 
   assert (len(shape) == nargs), "fcompute does not match output dimension"
---
+++ heterocl/python/heterocl/config.py
@@ -0,0 +1,2 @@
+
+init_dtype = "int32"
