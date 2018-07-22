e96fba74a1df68a34497bf4f0a45528dad3049e5
Huyuwei <huyuwei1995@gmail.com>
2018-07-22 19:41:01 -0400

    temp save test

--- heterocl/tests/test_schedule.py
+++ heterocl/tests/test_schedule.py
@@ -1,9 +1,10 @@
 import heterocl as hcl
+import tvm
 
 def test_schedule_pipeline():
-  initiation_interval = 3
-  a = hcl.placeholder((100,))
-  b = hcl.placeholder((100,))
+  initiation_interval = 2
+  a = hcl.placeholder((10,))
+  b = hcl.placeholder((10,))
   c = hcl.compute(a.shape, lambda i: a[i] + b[i])
   s = hcl.create_schedule(c)
   s[c].pipeline(c.axis[0], initiation_interval)
@@ -12,9 +13,9 @@ def test_schedule_pipeline():
   assert pipeline_hint_str in str(ir)
 
 def test_schedule_unroll():
-  factor = 4
-  a = hcl.placeholder((100,))
-  b = hcl.placeholder((100,))
+  factor = 2
+  a = hcl.placeholder((10,))
+  b = hcl.placeholder((10,))
   c = hcl.compute(a.shape, lambda i: a[i] + b[i])
   s = hcl.create_schedule(c)
   s[c].unroll(c.axis[0], factor=factor)
@@ -43,9 +44,30 @@ def test_schedule_reorder_loops():
   assert str(ir.body.body.body.body).startswith("for (j, 0, 20)")
   assert str(ir.body.body.body.body.body).startswith("for (l, 0, 40)")
 
+def test_schedule_compute_at():
+  a = hcl.placeholder((10, 20))
+  b = hcl.placeholder((10, 20))
+  c = hcl.compute(a.shape, lambda i, j: a[i, j] + b[i, j])
+  d = hcl.compute(c.shape, lambda i, j: c[i, j])
+  s = hcl.create_schedule(d)
+  s[c].compute_at(s[d], d.axis[1])
+  ir = hcl.lower(s, [a, b, d])
+  print ir
+
+def test_schedule_compute_at_tvm():
+  a = tvm.placeholder((10, 20))
+  b = tvm.placeholder((10, 20))
+  c = tvm.compute(a.shape, lambda i, j: a[i, j] + b[i, j])
+  d = tvm.compute(c.shape, lambda i, j: c[i, j])
+  s = tvm.create_schedule(d.op)
+  s[c].compute_at(s[d], d.op.axis[1])
+  ir = tvm.lower(s, [a, b, d], simple_mode=True)
+  print ir
 
 if __name__ == '__main__':
-  test_schedule_pipeline()
-  test_schedule_unroll()
-  test_schedule_fuse_loops()
-  test_schedule_reorder_loops()
+  # test_schedule_pipeline()
+  # test_schedule_unroll()
+  # test_schedule_fuse_loops()
+  # test_schedule_reorder_loops()
+  test_schedule_compute_at()
+  test_schedule_compute_at_tvm()
