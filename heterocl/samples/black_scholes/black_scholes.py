7fed4361e4fe70fe069fb0e2648cf3258071455d
Ziyan <jzfengziyan@gmail.com>
2018-08-09 08:56:12 +0800

    add some benchmarks

---
+++ heterocl/samples/black_scholes/black_scholes.py
@@ -0,0 +1,187 @@
+"""
+This is the black_scholes option pricing model.
+A basic model in finance used to decide the price of option.
+The model is as follow:
+
+C=S*N(d1)-X*exp(-rT)N(d2)
+P=X*exp(-rT)N(-d2)-S*N(-d1)
+
+where:
+d1=(ln(S/X)+(r+sigma^2/2)T)/(sigma*sqrt(T)
+d2=d1-sigma*sqrt(T)
+
+C:the output: Bullish price
+P:the output: Down price
+X:Option execution Price
+S:Current price of financial assets
+r:Risk-free continuous compound interest rate
+sigma:Annual volatility rate of stock continuous compounding rate
+N():Cumulative probability distribution function of unit normal distribution
+
+More information could be found here:https://en.wikipedia.org/wiki/Black%E2%80%93Scholes_model
+
+=================================
+"""
+#encoding:utf-8
+import numpy as np
+import heterocl as hcl
+hcl.config.init_dtype = "float32"
+#----------------------------------------
+#The input and output parameters.
+#xdiv1 is d1;xdiv2 is d2
+#d1Local is N(d1); d2local is N(d2)
+X = hcl.placeholder((1,))
+T = hcl.placeholder((1,))
+S = hcl.placeholder((1,))
+r = hcl.placeholder((1,))
+sigma = hcl.placeholder((1,))
+C = hcl.placeholder((1,))
+P = hcl.placeholder((1,))
+xdiv1 = hcl.placeholder((1,))
+xdiv2 = hcl.placeholder((1,))
+d1Local = hcl.placeholder((1,))
+d2Local = hcl.placeholder((1,))
+#------------------------------------------#
+with hcl.stage() as s:
+	xlogterm = hcl.local(0)
+	xlogterm[0] = hcl.log(S[0]/X[0])
+	xpowerterm = hcl.local(0)
+	xpowerterm[0] = 0.5*sigma[0]*sigma[0]
+	xnum = hcl.local(0)
+	xnum[0] = xlogterm[0]+(r[0]+xpowerterm[0])*T[0]
+	xsqrtterm = hcl.local(0)
+	xsqrtterm[0] = hcl.sqrt(T[0])
+	xden = hcl.local(0)
+	xden[0] = sigma[0]*xsqrtterm[0]
+	#xdiv1 = hcl.local(0)
+	xdiv1[0] = xnum[0]/xden[0]
+	#xdiv2 = hcl.local(0)
+	xdiv2[0] = xdiv1[0]-xden[0]
+	futurevaluex = hcl.local(0)
+	futurevaluex[0] = X[0]*hcl.exp(-r[0]*T[0])
+	#--------------------------------------------------#
+	#Calculate N(d1), also N(-d1)=1 - N(d1)
+	d1NPrimeofX = hcl.local(0)
+	d1NPrimeofX[0] = hcl.exp(-(xdiv1[0]*xdiv1[0])*0.5)*0.39894228040143270286
+	d1K2 = hcl.local(0)
+	d1K2[0] = 1/((xdiv1[0]*0.2316419)+1.0)
+	d1K2_2 = hcl.local(0)
+	d1K2_2[0] = d1K2[0]*d1K2[0]
+	d1K2_3 = hcl.local(0)
+	d1K2_3[0] = d1K2_2[0] * d1K2[0]
+	d1K2_4 = hcl.local(0)
+	d1K2_4[0] = d1K2_3[0] * d1K2[0]
+	d1K2_5 = hcl.local(0)
+	d1K2_5[0] = d1K2_4[0] * d1K2[0]
+	
+	d1Local_10 = hcl.local(0)
+	d1Local_10[0] = d1K2[0] * 0.319381530
+	d1Local_20 = hcl.local(0)
+	d1Local_20[0] = d1K2_2[0] * -0.356563782
+	d1Local_30 = hcl.local(0)
+	d1Local_30[0] = d1K2_3[0] * 1.781477937
+	d1Local_31 = hcl.local(0)
+	d1Local_31[0] = d1K2_4[0] * -1.821255978
+	d1Local_32 = hcl.local(0)
+	d1Local_32[0] = d1K2_5[0] * 1.330274429
+	
+	d1Local_21 = hcl.local(0)
+	d1Local_21[0] = d1Local_20[0] + d1Local_30[0]
+	d1Local_22 = hcl.local(0)
+	d1Local_22[0] = d1Local_21[0] + d1Local_31[0]
+	d1Local_23 = hcl.local(0)
+	d1Local_23[0] = d1Local_22[0] + d1Local_32[0]
+	d1Local_1 = hcl.local(0)
+	d1Local_1[0] = d1Local_23[0] + d1Local_10[0]
+
+	d1Local0 = hcl.local(0)
+	d1Local0[0] = d1Local_1[0] * d1NPrimeofX[0]
+	
+	#d1Local  = hcl.local(0)
+	d1Local[0]  = -d1Local0[0] + 1.0
+	#---------------------------------------------#
+	#Calculate N(d2), also N(-d2)=1 - N(d1)
+	d2NPrimeofX = hcl.local(0)
+	#1/sqrt(2*pi)=0.39894228040143270286
+	d2NPrimeofX[0] = (hcl.exp(-(xdiv2[0]*xdiv2[0])*0.5))*0.39894228040143270286 
+	d2K2 = hcl.local(0)
+	d2K2[0] = 1/((xdiv2[0]*0.2316419)+1.0)
+	d2K2_2 = hcl.local(0)
+	d2K2_2[0] = d2K2[0]*d2K2[0]
+	d2K2_3 = hcl.local(0)
+	d2K2_3[0] = d2K2_2[0] * d2K2[0]
+	d2K2_4 = hcl.local(0)
+	d2K2_4[0] = d2K2_3[0] * d2K2[0]
+	d2K2_5 = hcl.local(0)
+	d2K2_5[0] = d2K2_4[0] * d2K2[0]
+	
+	d2Local_10 = hcl.local(0)
+	d2Local_10[0] = d2K2[0] * 0.319381530
+	d2Local_20 = hcl.local(0)
+	d2Local_20[0] = d2K2_2[0] * -0.356563782
+	d2Local_30 = hcl.local(0)
+	d2Local_30[0] = d2K2_3[0] * 1.781477937
+	d2Local_31 = hcl.local(0)
+	d2Local_31[0] = d2K2_4[0] * -1.821255978
+	d2Local_32 = hcl.local(0)
+	d2Local_32[0] = d2K2_5[0] * 1.330274429
+	
+	d2Local_21 = hcl.local(0)
+	d2Local_21[0] = d2Local_20[0] + d2Local_30[0]
+	d2Local_22 = hcl.local(0)
+	d2Local_22[0] = d2Local_21[0] + d2Local_31[0]
+	d2Local_23 = hcl.local(0)
+	d2Local_23[0] = d2Local_22[0] + d2Local_32[0]
+	d2Local_1 = hcl.local(0)
+	d2Local_1[0] = d2Local_23[0] + d2Local_10[0]
+
+	d2Local0 = hcl.local(0)
+	d2Local0[0] = d2Local_1[0] * d2NPrimeofX[0]
+	
+	#d2Local  = hcl.local(0)
+	d2Local[0]  = -d2Local0[0] + 1.0
+	#---------------------------------------------------#
+	#Calculate C and P
+	C[0] = (S[0]*d1Local[0]) - (futurevaluex[0]*d2Local[0])
+	P[0] = (futurevaluex[0]*d2Local0[0]) - (S[0]*d1Local0[0])
+	#------------------------------------------------------------#
+	
+o = hcl.create_schedule(s)
+print hcl.lower(o, [X,T,S,r,sigma,C,P,xdiv1,xdiv2,d1Local,d2Local])
+f = hcl.build(o, [X,T,S,r,sigma,C,P,xdiv1,xdiv2,d1Local,d2Local])
+
+#---------------------------------------------------------------------#
+# Give an example set of parameters
+hcl_X = hcl.asarray([165], dtype = hcl.Float())
+hcl_T = hcl.asarray([0.0959], dtype = hcl.Float())
+hcl_S = hcl.asarray([164], dtype = hcl.Float())
+hcl_r = hcl.asarray([0.0521], dtype = hcl.Float())
+hcl_sigma = hcl.asarray([0.29], dtype = hcl.Float())
+hcl_C = hcl.asarray([0], dtype = hcl.Float())
+hcl_P = hcl.asarray([0], dtype = hcl.Float())
+hcl_xdiv1 = hcl.asarray([0], dtype = hcl.Float())
+hcl_xdiv2 = hcl.asarray([0], dtype = hcl.Float())
+hcl_d1Local = hcl.asarray([0], dtype = hcl.Float())
+hcl_d2Local = hcl.asarray([0], dtype = hcl.Float())
+
+f(hcl_X,hcl_T,hcl_S,hcl_r,hcl_sigma,hcl_C,hcl_P,hcl_xdiv1,hcl_xdiv2,hcl_d1Local,hcl_d2Local)
+
+print("Bullish price:")
+print hcl_C
+print("Down price:")
+print hcl_P
+print("value of d1:")
+print hcl_xdiv1
+print("value of d2:")
+print hcl_xdiv2
+print("value of N(d1):")
+print hcl_d1Local
+print("value of N(d2):")
+print hcl_d2Local
+
+	
+	
+	
+	
+	
+
