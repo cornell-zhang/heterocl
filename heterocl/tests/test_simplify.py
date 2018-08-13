ea3ce41730939184c4407b69e8945c6ea4284df9
seanlatias <seanlatias@gmail.com>
2018-08-13 11:59:56 -0400

    [Add] pass to remove single loop

---
+++ heterocl/tests/test_simplify.py
@@ -0,0 +1,8 @@
+import heterocl as hcl
+
+def test_remove_single_loop():
+  a = hcl.placeholder((1,))
+  b = hcl.compute(a.shape, lambda x: a[x] + 1)
+  s = hcl.create_schedule(b)
+  ir = hcl.lower(s, [a, b])
+  assert "for (x, 0, 1)" not in str(ir)
