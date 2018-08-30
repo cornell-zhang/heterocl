f513460d89baba0ef8eaf101d9843085a4da123a
seanlatias <seanlatias@gmail.com>
2018-08-30 10:01:40 -0400

    [Add] new tests for pack/unpack

--- heterocl/tests/test_api.py
+++ heterocl/tests/test_api.py
@@ -128,6 +128,36 @@ def test_unpack():
         golden = (numA >> (i/4*k)) % (1 << (i/4))
         assert numB == golden
 
+def test_unpack_dtype():
+
+  def unpack(A, B):
+    C = hcl.unpack(A, name = "C", dtype = B.dtype)
+    U = hcl.update(B, lambda x: C[x])
+
+    return U
+
+  for i in range(4, 36, 4):
+    A = hcl.placeholder((10,), "A", dtype = hcl.UInt(i))
+    B = hcl.placeholder((40,), "B", dtype = hcl.UInt(i/4))
+
+    s = hcl.make_schedule([A, B], unpack)
+    f = hcl.build(s, [A, B])
+
+    _A = hcl.asarray(np.random.randint(1000, size = (10,)), dtype = hcl.UInt(i))
+    _B = hcl.asarray(np.zeros(40), dtype = hcl.UInt(i/4))
+
+    f(_A, _B)
+
+    __A = _A.asnumpy()
+    __B = _B.asnumpy()
+
+    for j in range(0, 10):
+      for k in range(0, 4):
+        numA = __A[j]
+        numB = __B[j*4 + k]
+        golden = (numA >> (i/4*k)) % (1 << (i/4))
+        assert numB == golden
+
 """
 Testing API: pack
 """
@@ -135,6 +165,35 @@ def test_pack():
 
   def pack(A, B):
     C = hcl.pack(A, factor = 4, name = "C")
+    return hcl.update(B, lambda x: C[x])
+
+  for i in range(4, 36, 4):
+    A = hcl.placeholder((40,), "A", dtype = hcl.UInt(i/4))
+    B = hcl.placeholder((10,), "B", dtype = hcl.UInt(i))
+
+    s = hcl.make_schedule([A, B], pack)
+    f = hcl.build(s, [A, B])
+
+    _A = hcl.asarray(np.random.randint(1000, size = (40,)), dtype = hcl.UInt(i/4))
+    _B = hcl.asarray(np.zeros(10), dtype = hcl.UInt(i))
+
+    f(_A, _B)
+
+    __A = _A.asnumpy()
+    __B = _B.asnumpy()
+
+    for j in range(0, 10):
+      golden = 0
+      numB = __B[j]
+      for k in range(0, 4):
+        numA = __A[j*4 + k]
+        golden += numA << (k * i/4)
+      assert numB == golden
+
+def test_pack_dtype():
+
+  def pack(A, B):
+    C = hcl.pack(A, name = "C", dtype = B.dtype)
     return hcl.update(B, lambda x: C[x])
 
   for i in range(4, 36, 4):
