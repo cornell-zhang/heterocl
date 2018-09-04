d72908a1791153fad1f67498573e679f332f81f4
seanlatias <seanlatias@users.noreply.github.com>
2018-09-04 11:53:30 -0400

    [Benchmark] Update all Merlin C related benchmark with kernel reuse (#48)
    * [Add] numpy pack and unpack APIs
    
    [Fix] incorrect getslice / setslice
    
    [Fix] did not clean the tensor's last_update
    
    [Fix] sgd benchmark now working
    
    * [Update] simplify sgd benchmark
    
    * [Update] sgd example now iterates 5 epochs
    
    * [Update] all merlinc required benchmarks with better code reuse

--- heterocl/samples/fft/fft.py
+++ heterocl/samples/fft/fft.py
@@ -2,9 +2,7 @@ import numpy as np
 import heterocl as hcl
 import tvm
 
-
 hcl.config.init_dtype = "float32"
-
 
 def PreCompute_BitReverse_Pattern(L):
   """
@@ -25,51 +23,52 @@ def PreCompute_BitReverse_Pattern(L):
     IndexTable[i] = int(b[::-1], 2)
   return IndexTable
 
-
-def build_fft(X_real, X_imag, IndexTable):
-  L = X_real.shape[0]
-  if np.log2(L) % 1 > 0:
-    raise ValueError("Length of input vector (1d tensor) must be power of 2")
-  num_stages = int(np.log2(L))
-
-  # bit reverse permutation
-  F_real = hcl.compute(X_real.shape, lambda i: X_real[IndexTable[i]], name='F_real')
-  F_imag = hcl.compute(X_imag.shape, lambda i: X_imag[IndexTable[i]], name='F_imag')
-
-  def FFT(F_real, F_imag):
-    one = hcl.local(1, dtype="int32")
-    with hcl.for_(0, num_stages) as stage:
-      DFTpts = one[0] << (stage + 1)
-      numBF = DFTpts / 2
-      e = -2 * np.pi / DFTpts
-      a = hcl.local(0)
-      with hcl.for_(0, numBF) as j:
-        c = hcl.local(tvm.cos(a[0]))
-        s = hcl.local(tvm.sin(a[0]))
-        a[0] = a[0] + e
-        with hcl.for_(j, L + DFTpts - 1, DFTpts) as i:
-          i_lower = i + numBF
-          temp_r = hcl.local(F_real[i_lower] * c - F_imag[i_lower] * s)
-          temp_i = hcl.local(F_imag[i_lower] * c + F_real[i_lower] * s)
-          F_real[i_lower] = F_real[i] - temp_r[0]
-          F_imag[i_lower] = F_imag[i] - temp_i[0]
-          F_real[i] = F_real[i] + temp_r[0]
-          F_imag[i] = F_imag[i] + temp_i[0]
-
-  with hcl.stage() as Out:
-    FFT(F_real, F_imag)
-
-  s = hcl.create_schedule(Out)
-  # print hcl.lower(s, [X_real, X_imag, IndexTable, F_real, F_imag])
-  f = hcl.build(s, [X_real, X_imag, IndexTable, F_real, F_imag])
-  return f
-
 L = 1024
-X_real = hcl.placeholder((L,))
-X_imag = hcl.placeholder((L,))
-IndexTable = hcl.placeholder((L,), dtype="int32")
 
-f = build_fft(X_real, X_imag, IndexTable)
+def top(target = None):
+
+  def fft(X_real, X_imag, IndexTable, F_real, F_imag):
+    L = X_real.shape[0]
+    if np.log2(L) % 1 > 0:
+      raise ValueError("Length of input vector (1d tensor) must be power of 2")
+    num_stages = int(np.log2(L))
+
+    # bit reverse permutation
+    hcl.update(F_real, lambda i: X_real[IndexTable[i]], name='F_real_update')
+    hcl.update(F_imag, lambda i: X_imag[IndexTable[i]], name='F_imag_update')
+
+    with hcl.stage() as Out:
+      one = hcl.local(1, dtype="int32")
+      with hcl.for_(0, num_stages) as stage:
+        DFTpts = one[0] << (stage + 1)
+        numBF = DFTpts / 2
+        e = -2 * np.pi / DFTpts
+        a = hcl.local(0)
+        with hcl.for_(0, numBF) as j:
+          c = hcl.local(tvm.cos(a[0]))
+          s = hcl.local(tvm.sin(a[0]))
+          a[0] = a[0] + e
+          with hcl.for_(j, L + DFTpts - 1, DFTpts) as i:
+            i_lower = i + numBF
+            temp_r = hcl.local(F_real[i_lower] * c - F_imag[i_lower] * s)
+            temp_i = hcl.local(F_imag[i_lower] * c + F_real[i_lower] * s)
+            F_real[i_lower] = F_real[i] - temp_r[0]
+            F_imag[i_lower] = F_imag[i] - temp_i[0]
+            F_real[i] = F_real[i] + temp_r[0]
+            F_imag[i] = F_imag[i] + temp_i[0]
+
+    return Out
+
+  X_real = hcl.placeholder((L,))
+  X_imag = hcl.placeholder((L,))
+  IndexTable = hcl.placeholder((L,), dtype="int32")
+  F_real = hcl.placeholder((L,))
+  F_imag = hcl.placeholder((L,))
+
+  s = hcl.make_schedule([X_real, X_imag, IndexTable, F_real, F_imag], fft)
+  return hcl.build(s, [X_real, X_imag, IndexTable, F_real, F_imag], target = target)
+
+f = top()
 
 x_real_np = np.random.random((L))
 x_imag_np = np.random.random((L))
--- heterocl/samples/fft/fft_merlin.py
+++ heterocl/samples/fft/fft_merlin.py
@@ -1,77 +1,14 @@
 import numpy as np
 import heterocl as hcl
-import tvm
+from fft import PreCompute_BitReverse_Pattern, top, L
 import os
-
-
-hcl.config.init_dtype = "float32"
-
-
-def PreCompute_BitReverse_Pattern(L):
-  """
-  Parameters
-  ----------
-  L : int
-    Length of the input vector.
-
-  Returns
-  -------
-  IndexTable : vector of int
-    Permutation pattern.
-  """
-  bit_width = int(np.log2(L))
-  IndexTable = np.zeros((L), dtype='int')
-  for i in range(L):
-    b = '{:0{width}b}'.format(i, width=bit_width)
-    IndexTable[i] = int(b[::-1], 2)
-  return IndexTable
-
-
-def build_fft(X_real, X_imag, IndexTable):
-  L = X_real.shape[0]
-  if np.log2(L) % 1 > 0:
-    raise ValueError("Length of input vector (1d tensor) must be power of 2")
-  num_stages = int(np.log2(L))
-
-  # bit reverse permutation
-  F_real = hcl.compute(X_real.shape, lambda i: X_real[IndexTable[i]], name='F_real')
-  F_imag = hcl.compute(X_imag.shape, lambda i: X_imag[IndexTable[i]], name='F_imag')
-
-  def FFT(F_real, F_imag):
-    one = hcl.local(1, dtype="int32")
-    with hcl.for_(0, num_stages) as stage:
-      DFTpts = one[0] << (stage + 1)
-      numBF = DFTpts / 2
-      e = -2 * np.pi / DFTpts
-      a = hcl.local(0)
-      with hcl.for_(0, numBF) as j:
-        c = hcl.local(tvm.cos(a[0]))
-        s = hcl.local(tvm.sin(a[0]))
-        a[0] = a[0] + e
-        with hcl.for_(j, L + DFTpts - 1, DFTpts) as i:
-          i_lower = i + numBF
-          temp_r = hcl.local(F_real[i_lower] * c - F_imag[i_lower] * s)
-          temp_i = hcl.local(F_imag[i_lower] * c + F_real[i_lower] * s)
-          F_real[i_lower] = F_real[i] - temp_r[0]
-          F_imag[i_lower] = F_imag[i] - temp_i[0]
-          F_real[i] = F_real[i] + temp_r[0]
-          F_imag[i] = F_imag[i] + temp_i[0]
-
-  with hcl.stage() as Out:
-    FFT(F_real, F_imag)
-
-  s = hcl.create_schedule(Out)
-  # print hcl.lower(s, [X_real, X_imag, IndexTable, F_real, F_imag])
-  f = hcl.build(s, [X_real, X_imag, IndexTable, F_real, F_imag],
-          target='merlinc')
-  return f
 
 L = 1024
 X_real = hcl.placeholder((L,))
 X_imag = hcl.placeholder((L,))
 IndexTable = hcl.placeholder((L,), dtype="int32")
 
-code = build_fft(X_real, X_imag, IndexTable)
+code = top('merlinc')
 with open('fft_kernel.cpp', 'w') as f:
     f.write(code)
 
