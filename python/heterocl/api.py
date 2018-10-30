a3dfdff15dcd2a3c1026d4f3d46ff2ff4c29bc02
seanlatias <seanlatias@gmail.com>
2018-10-30 16:34:33 -0400

    fix indentation of comments

--- python/heterocl/api.py
+++ python/heterocl/api.py
@@ -33,27 +33,27 @@ def init(init_dtype="int32"):
     --------
     .. code-block:: python
 
-    # app 1
-    hcl.init()
-    A = hcl.placeholder(...)
-    B = hcl.placeholder(...)
-    def app1(A, B):
-        # define the algorithm for app 1
-    s = hcl.create_scheme([A, B], app1)
-    # apply customization primitives
-    f1 = hcl.build(s)
-    # execute f1
+        # app 1
+        hcl.init()
+        A = hcl.placeholder(...)
+        B = hcl.placeholder(...)
+        def app1(A, B):
+            # define the algorithm for app 1
+        s = hcl.create_scheme([A, B], app1)
+        # apply customization primitives
+        f1 = hcl.build(s)
+        # execute f1
 
-    # app 2 - initialize again with a different data type
-    hcl.init(hcl.Float())
-    A = hcl.placeholder(...)
-    B = hcl.placeholder(...)
-    C = hcl.placeholder(...)
-    def app2(A, B, C):
-        # define the algorithm for app 2
-    s = hcl.create_scheme([A, B, C], app2)
-    f2 = hcl.build(s)
-    # execute f2
+        # app 2 - initialize again with a different data type
+        hcl.init(hcl.Float())
+        A = hcl.placeholder(...)
+        B = hcl.placeholder(...)
+        C = hcl.placeholder(...)
+        def app2(A, B, C):
+            # define the algorithm for app 2
+        s = hcl.create_scheme([A, B, C], app2)
+        f2 = hcl.build(s)
+        # execute f2
 
     Parameters
     ----------
