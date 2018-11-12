d32e0f50c036cc6a88b62ed096db9bb5d576da1e
Yuwei Hu <huyuwei1995@gmail.com>
2018-11-12 10:29:19 -0500

    [Backend] Code generator for Vivado HLS (#66)
    * init support for Vivado HLS Code generator
    
    * support ap_int and ap_fixed; support unroll and pipeline pragmas
    
    * fix indentation

---
+++ tvm/src/codegen/codegen_vhls.h
@@ -0,0 +1,31 @@
+/*!
+ *  Copyright (c) 2018 by Contributors
+ * \file codegen_vhls.h
+ * \brief Generate Vivado HLS kernel code.
+ */
+#ifndef TVM_CODEGEN_CODEGEN_VHLS_H_
+#define TVM_CODEGEN_CODEGEN_VHLS_H_
+
+#include <tvm/codegen.h>
+#include <tvm/packed_func_ext.h>
+#include <string>
+#include "./codegen_c.h"
+#include "./merlinc/codeanalys_merlinc.h"
+
+namespace tvm {
+namespace codegen {
+
+class CodeGenVivadoHLS final : public CodeGenC {
+ public:
+  void AddFunction(LoweredFunc f, str2tupleMap<std::string, Type> map_arg_type);
+  void PrintType(Type t, std::ostream& os) override;
+
+  void VisitStmt_(const For* op) override;
+  void VisitStmt_(const LetStmt* op) override;
+  void VisitStmt_(const IfThenElse* op) override;
+};
+
+}  // namespace codegen
+}  // namespace tvm
+
+#endif  // TVM_CODEGEN_CODEGEN_VHLS_H_
