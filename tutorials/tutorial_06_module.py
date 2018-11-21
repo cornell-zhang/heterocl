108e20e06148ad643f268ae838dfbd249336e8f6
seanlatias <seanlatias@gmail.com>
2018-11-21 16:54:41 -0500

    add tutorial for modules

--- tutorials/tutorial_06_module.py
+++ tutorials/tutorial_06_module.py
@@ -26,7 +26,7 @@ hcl.init()
 
 def maximum(A, B, C, D):
 
-    @hcl.module([(10,), (10,), ()])
+    @hcl.module([A.shape, B.shape, ()])
     def find_max(A, B, x):
         with hcl.if_(A[x] > B[x]):
             hcl.return_(A[x])
@@ -83,8 +83,110 @@ hcl_O = hcl.asarray(o, dtype=hcl.Int())
 
 f(hcl_A, hcl_B, hcl_C, hcl_D, hcl_O)
 
+print("Input tensors:")
 print(hcl_A)
 print(hcl_B)
 print(hcl_C)
 print(hcl_D)
+print("Output tensor:")
 print(hcl_O)
+
+def test1():
+    m1 = np.maximum(a, b)
+    m2 = np.maximum(c, d)
+    m = np.maximum(m1, m2)
+    assert np.array_equal(hcl_O.asnumpy(), m)
+
+test1()
+
+##############################################################################
+# Modules Without Return Statement
+# --------------------------------
+# HeteroCL also allows users to define modules without a return statement. The
+# usage is exactly the same as what we just introduced. The only differece is
+# that the module can be called in a stand-alone way. Namely, it does not need
+# to be contained in any HeteroCL APIs. Let's use the same example of finding
+# the maximum. However, this time we update the output directly.
+
+hcl.init()
+
+def maximum2(A, B, C, D):
+
+    # B will be the tensor that holds the maximum values
+    @hcl.module([A.shape, B.shape])
+    def find_max(A, B):
+        with hcl.for_(0, A.shape[0]) as i:
+            with hcl.if_(A[i] > B[i]):
+                B[i] = A[i]
+
+    find_max(A, B)
+    find_max(C, D)
+    find_max(B, D)
+
+s = hcl.create_schedule([A, B, C, D], maximum2)
+f = hcl.build(s)
+
+##############################################################################
+# In the above example, we can see that now without the return value, we can
+# directly call the defined module. Let's check the results. They should be
+# the same as our first example.
+
+f(hcl_A, hcl_B, hcl_C, hcl_D)
+
+print("Output tensor:")
+print(hcl_D)
+
+def test2():
+    m1 = np.maximum(a, b)
+    m2 = np.maximum(c, d)
+    m = np.maximum(m1, m2)
+    assert np.array_equal(hcl_D.asnumpy(), m)
+
+test2()
+
+##############################################################################
+# Data Type Customization for Modules
+# -----------------------------------
+# We can also apply data type customization to our defined modules. There are
+# two ways to do that. First, you can specify the data types directly in the
+# module decorator. Second, you can use the ``quantize`` and ``downsize`` APIs.
+# Let's show how we can downsize the first example.
+
+A = hcl.placeholder((10,), dtype=hcl.UInt(4))
+B = hcl.placeholder((10,), dtype=hcl.UInt(4))
+C = hcl.placeholder((10,), dtype=hcl.UInt(4))
+D = hcl.placeholder((10,), dtype=hcl.UInt(4))
+
+s = hcl.create_scheme([A, B, C, D], maximum)
+# Downsize the input arguments and also the return value
+s.downsize([maximum.find_max.A, maximum.find_max.B, maximum.find_max], hcl.UInt(4))
+# We also need to downsize the intermediate results
+s.downsize([maximum.max_1, maximum.max_2], hcl.UInt(4))
+s = hcl.create_schedule_from_scheme(s)
+f = hcl.build(s)
+
+##############################################################################
+# Let's run it.
+
+hcl_A = hcl.asarray(a, hcl.UInt(4))
+hcl_B = hcl.asarray(b, hcl.UInt(4))
+hcl_C = hcl.asarray(c, hcl.UInt(4))
+hcl_D = hcl.asarray(d, hcl.UInt(4))
+hcl_O = hcl.asarray(o)
+
+f(hcl_A, hcl_B, hcl_C, hcl_D, hcl_O)
+
+print("Downsized output tensor:")
+print(hcl_O)
+
+##############################################################################
+# We can see that the results are downsized to 4-bit numbers. We can double
+# check this.
+
+def test3():
+    m1 = np.maximum(a%16, b%16)
+    m2 = np.maximum(c%16, d%16)
+    m = np.maximum(m1%16, m2%16)
+    assert np.array_equal(hcl_O.asnumpy(), m)
+
+test3()
