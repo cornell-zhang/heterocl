5064372e8cdc9bdf1024bdd3fc703269ccff0d68
seanlatias <seanlatias@gmail.com>
2019-01-25 16:14:29 -0500

    add attrs for nn layers

--- samples/lenet/lenet.py
+++ samples/lenet/lenet.py
@@ -1,5 +1,6 @@
 import heterocl as hcl
 import heterocl.tvm as tvm
+import hlib
 import numpy as np
 
 dtype = "float32"
@@ -150,19 +151,19 @@ batch_size = 1000
 
 def build_lenet(input_image, weight_conv1, weight_conv2, weight_fc1, weight_fc2, lenet):
     # first conv
-    conv1 = conv2d_nchw(input_image, weight_conv1)
-    tanh1 = tanh_4d(conv1, "tanh1")
-    pool1 = max_pool(tanh1, kernel=(2,2), stride=(2,2))
+    conv1 = hlib.nn.conv2d_nchw(input_image, weight_conv1)
+    tanh1 = hlib.nn.tanh(conv1, "tanh1")
+    pool1 = hlib.nn.max_pool(tanh1, kernel=(2,2), stride=(2,2))
     # second conv
-    conv2 = conv2d_nchw(pool1, weight_conv2)
-    tanh2 = tanh_4d(conv2, "tanh2")
-    pool2 = max_pool(tanh2, kernel=(2,2), stride=(2,2))
+    conv2 = hlib.nn.conv2d_nchw(pool1, weight_conv2)
+    tanh2 = hlib.nn.tanh(conv2, "tanh2")
+    pool2 = hlib.nn.max_pool(tanh2, kernel=(2,2), stride=(2,2))
     # first fc
-    flat = flatten(pool2)
-    fc1 = dense(flat, weight_fc1)
-    tanh3 = tanh_2d(fc1, "tanh3")
+    flat = hlib.nn.flatten(pool2)
+    fc1 = hlib.nn.dense(flat, weight_fc1)
+    tanh3 = hlib.nn.tanh(fc1, "tanh3")
     # second fc
-    fc2 =  dense(tanh3, weight_fc2)
+    fc2 =  hlib.nn.dense(tanh3, weight_fc2)
     # loss
     return softmax(lenet, fc2)
 
