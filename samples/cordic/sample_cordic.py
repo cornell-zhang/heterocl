ba78aecc2ead125285248bd239103d4b5806250f
seanlatias <seanlatias@gmail.com>
2019-05-02 16:42:43 -0400

    update CORDIC

--- samples/cordic/sample_cordic.py
+++ samples/cordic/sample_cordic.py
@@ -4,18 +4,18 @@ HeteroCL Tutorial : CORDIC Design
 
 **Author**: Yi-Hsiang Lai (seanlatias@github)
 
-COordinate Rotation DIgital Computer (CORDIC) is a method for calculating a variety of
-functions including trigonometric and hyperbolic. The various functions are calculated
-through an iterative set of vector rotations. At the end of these rotations, the value of
-the function is easily determined from the (x, y) coordinate. A CORDIC is often used to
-achieve low-cost multiplierless sine/cosine implementations in FPGA as well as ASIC
-designs.
+COordinate Rotation DIgital Computer (CORDIC) is a method for calculating a
+variety of functions including trigonometric and hyperbolic. The various
+functions are calculated through an iterative set of vector rotations. At the
+end of these rotations, the value of the function is easily determined from
+the (x, y) coordinate. A CORDIC is often used to achieve low-cost
+multiplierless sine/cosine implementations in FPGA as well as ASIC designs.
 
-In this tutorial, we demonstrate how to make use of the decoupled quantization schemes
-and algorithms in HeteroCL. We also show how we can explore different quantization
-schemes with the quantize API.
+In this tutorial, we demonstrate how to make use of the decoupled quantization
+schemes and algorithms in HeteroCL. We also show how we can explore different
+quantization schemes with the quantize API.
 """
-###########################################################################################
+##############################################################################
 # Import modules and set constants.
 
 import heterocl as hcl
@@ -23,36 +23,43 @@ import numpy as np
 import math
 import os
 
-cordic_ctab = [0.78539816339744828000,0.46364760900080609000,0.24497866312686414000,
-    0.12435499454676144000,0.06241880999595735000,0.03123983343026827700,0.01562372862047683100,
-    0.00781234106010111110,0.00390623013196697180,0.00195312251647881880,0.00097656218955931946,
-    0.00048828121119489829,0.00024414062014936177,0.00012207031189367021,0.00006103515617420877,
-    0.00003051757811552610,0.00001525878906131576,0.00000762939453110197,0.00000381469726560650,
-    0.00000190734863281019,0.00000095367431640596,0.00000047683715820309,0.00000023841857910156,
-    0.00000011920928955078,0.00000005960464477539,0.00000002980232238770,0.00000001490116119385,
-    0.00000000745058059692,0.00000000372529029846,0.00000000186264514923,0.00000000093132257462,
-    0.00000000046566128731,0.00000000023283064365,0.00000000011641532183,0.00000000005820766091,
-    0.00000000002910383046,0.00000000001455191523,0.00000000000727595761,0.00000000000363797881,
-    0.00000000000181898940,0.00000000000090949470,0.00000000000045474735,0.00000000000022737368,
-    0.00000000000011368684,0.00000000000005684342,0.00000000000002842171,0.00000000000001421085,
-    0.00000000000000710543,0.00000000000000355271,0.00000000000000177636,0.00000000000000088818,
-    0.00000000000000044409,0.00000000000000022204,0.00000000000000011102,0.00000000000000005551,
-    0.00000000000000002776,0.00000000000000001388,0.00000000000000000694,0.00000000000000000347,
-    0.00000000000000000173,0.00000000000000000087,0.00000000000000000043,0.00000000000000000022]
+cordic_ctab = [
+        0.78539816339744828000,0.46364760900080609000,0.24497866312686414000,
+        0.12435499454676144000,0.06241880999595735000,0.03123983343026827700,
+        0.01562372862047683100,0.00781234106010111110,0.00390623013196697180,
+        0.00195312251647881880,0.00097656218955931946,0.00048828121119489829,
+        0.00024414062014936177,0.00012207031189367021,0.00006103515617420877,
+        0.00003051757811552610,0.00001525878906131576,0.00000762939453110197,
+        0.00000381469726560650,0.00000190734863281019,0.00000095367431640596,
+        0.00000047683715820309,0.00000023841857910156,0.00000011920928955078,
+        0.00000005960464477539,0.00000002980232238770,0.00000001490116119385,
+        0.00000000745058059692,0.00000000372529029846,0.00000000186264514923,
+        0.00000000093132257462,0.00000000046566128731,0.00000000023283064365,
+        0.00000000011641532183,0.00000000005820766091,0.00000000002910383046,
+        0.00000000001455191523,0.00000000000727595761,0.00000000000363797881,
+        0.00000000000181898940,0.00000000000090949470,0.00000000000045474735,
+        0.00000000000022737368,0.00000000000011368684,0.00000000000005684342,
+        0.00000000000002842171,0.00000000000001421085,0.00000000000000710543,
+        0.00000000000000355271,0.00000000000000177636,0.00000000000000088818,
+        0.00000000000000044409,0.00000000000000022204,0.00000000000000011102,
+        0.00000000000000005551,0.00000000000000002776,0.00000000000000001388,
+        0.00000000000000000694,0.00000000000000000347,0.00000000000000000173,
+        0.00000000000000000087,0.00000000000000000043,0.00000000000000000022]
 
 K_const = 0.6072529350088812561694
 
+##############################################################################
 # Main Algorithm
-# =========================================================================================
-# We let the data type be the input argument of our top function. This is how we can set
-# different quantization schemes.
+# ==============
+# We let the data type be the input argument of our top function. This is how
+# we can set different quantization schemes.
 def cordic(X, Y, C, theta, N):
 
     # Prepare all input values and intermediate variables.
     T = hcl.compute((1,), lambda x: 0, "T", X.dtype)
     current = hcl.compute((1,), lambda x: 0, "current", X.dtype)
 
-    # This is the main loop body. The more steps we iterate, the better accuracy we get.
+    # Main loop body: The more steps we iterate, the better accuracy we get.
     def step_loop(step):
         with hcl.if_(theta[0] > current[0]):
             T[0] = X[0] - (Y[0] >> step)
@@ -68,16 +75,18 @@ def cordic(X, Y, C, theta, N):
     # This is the main computation that calls the loop body.
     hcl.mutate((N,), lambda step: step_loop(step), "calc")
 
-# End of Main Algorithm
-###########################################################################################
 
-# Set the range of the angle we want to test. Also set the number of iterations.
+###############################################################################
+# Test with Different Data Types
+# ==============================
+#
+# Set the range of the angle we want to test and set the number of iterations.
 NUM = 90
 _N = 60
-# For checking the results
 from .cordic_golden import golden
 
-# Loop through different bit-widths and build different top functions accordingly.
+###############################################################################
+# Loop through different bit-widths.
 for b in range(2, 64, 4):
 
     dtype = hcl.Fixed(b, b-2)
