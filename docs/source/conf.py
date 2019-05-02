69d6eb054a420b91580ff8d38b4b8d7d8a9e1832
seanlatias <seanlatias@gmail.com>
2019-05-02 16:31:50 -0400

    update conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -189,7 +189,7 @@ from sphinx_gallery.sorting import FileNameSortKey
 sphinx_gallery_conf = {
         'examples_dirs': ['../../samples', '../../tutorials'],
         'gallery_dirs': ['samples', 'tutorials'],
-        'ignore_pattern': '(.*data\.py)|(.*merlinc\.py)|(example.*py)|(test.*py)|(__init__.py)',
+        'ignore_pattern': '(.*data\.py)|(.*merlinc\.py)|(example.*py)|(test.*py)|(__init__.py)|(.*golden.*py)',
         'filename_pattern': '(sample.*.py)|(tutorial.*py)',
         'within_subsection_order': FileNameSortKey,
         'download_all_examples': False,
