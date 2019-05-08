a34626d4df5691c85d962277d5208754a458d0e8
seanlatias <seanlatias@gmail.com>
2019-05-08 17:14:25 -0400

    update conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -98,7 +98,10 @@ html_theme = 'sphinx_rtd_theme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+        'logo_only' : True,
+        'style_nav_header_background': 'LightBlue'
+}
 
 html_logo = '_static/images/hcl_logo.png'
 
