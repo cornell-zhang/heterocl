d794ee3f806b2a0510aa4b6e95b957699c4f2b54
seanlatias <seanlatias@gmail.com>
2019-05-08 16:19:19 -0400

    add logo to doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -100,6 +100,8 @@ html_theme = 'sphinx_rtd_theme'
 #
 # html_theme_options = {}
 
+html_logo = '_static/images/hcl_logo.png'
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
