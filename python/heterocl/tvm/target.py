e687bfb1908a54dcc28bf4e3ae3551c1c48d0a54
hgyhungry <ninahuangguyue@hotmail.com>
2019-08-07 17:15:42 -0400

    modified:   python/heterocl/tvm/target.py

--- python/heterocl/tvm/target.py
+++ python/heterocl/tvm/target.py
@@ -50,7 +50,7 @@ except ImportError as err_msg:
     if _LIB_NAME != "libhcl_runtime.so":
         raise err_msg
 
-FPGA_TARGETS = ['merlinc', 'soda', 'soda_xhls', 'vhls', 'ihls', 'vhls_csim']
+FPGA_TARGETS = ['merlinc', 'soda', 'soda_xhls', 'vhls', 'ihls', 'vhls_csim', 'rv64_ppac']
 
 def _merge_opts(opts, new_opts):
     """Helper function to merge options"""
@@ -68,7 +68,7 @@ class Target(object):
 
     Parameters
     ----------
-    target_name : {"llvm", "cuda", "opencl", "metal", "rocm", "stackvm", "opengl", "ext_dev"}
+    target_name : {"llvm", "cuda", "opencl", "metal", "rocm", "stackvm", "opengl", "ext_dev", "rv64_ppac"}
         The major target name.
 
                   {"merlinc", "soda", "soda_xhls", "vhls"}
