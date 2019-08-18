1f5fbb3560df8273ba675289155353be75921f8c
ybai62868 <ybai62868@gmail.com>
2019-08-18 16:57:59 -0400

    no bug

--- tvm/src/codegen/opencl/codegen_opencl.h
+++ tvm/src/codegen/opencl/codegen_opencl.h
@@ -3,8 +3,8 @@
     yb269@cornell.edu
 */
 
-#ifndef TVM_CODEGEN_CODEGEN_SDACCEL_H_
-#define TVM_CODEGEN_CODEGEN_SDACCEL_H_
+#ifndef TVM_CODEGEN_CODEGEN_OPENCL_H_
+#define TVM_CODEGEN_CODEGEN_OPENCL_H_
 
 # include <tvm/codegen.h>
 # include <tvm/packed_func_ext.h>
@@ -16,36 +16,37 @@ namespace TVM {
 namespace codegen {
 
 
-class CodeGenOpenCL final : public CodeGenC {
+class CodeGenOpenCL : public CodeGenC {
     public:
         CodeGenOpenCL();
         // void AddFunction(LoweredFunc f);
         void AddFunction(LoweredFunc f, str2tupleMap<std::string, Type> map_arg_type);
         std::string Finish();
 
-        void InitFuncState(LoweredFunc f) final;
-        void BindThreadIndex(const IterVar& iv) final; // NOLINT(*)
-        void PrintStorageScope(const std::string& scope, std::ostream& os) final; //NOLINT(*)
-        void PrintStorageSync(const Call* op) final; //NOLINT(*)
-        // void PrintType(Type t, std::ostream& os) final; //NOLINT(*)
+        void InitFuncState(LoweredFunc f) override;
+        void BindThreadIndex(const IterVar& iv) override; // NOLINT(*)
+        void PrintStorageScope(const std::string& scope, std::ostream& os) override; //NOLINT(*)
+        void PrintStorageSync(const Call* op) override; //NOLINT(*)
+        // void PrintType(Type t, std::ostream& os) override; //NOLINT(*)
         void PrintType(Type t, std::ostream& os) override; //NOLINT(*)
 
         std::string GetVecLoad(Type t, const Variable * buffer, 
-                               Expr base) final; // NOLINT(*)
+                               Expr base) override; // NOLINT(*)
         void PrintVecStore(const Variable * buffer, Type t,
-                           Expr base, const std::string& value) final; //NOLINT(*)
+                           Expr base, const std::string& value) override; //NOLINT(*)
         void PrintVecAddr(const Variable * buffer, Type t,
                           Expr base, std::ostream& os); //NOLINT(*)
         std::string CastFromTo(std::string value, Type from, Type target) override; //NOLINT(*)
     
         //overload visitor
-        void VisitExpr_(const Broadcast * op, std::ostream& os) final; //NOLINT(*)
-        void VisitExpr_(const Call * op, std::ostream& os) final; //NOLINT(*)
-        void VisitExpr_(const Select * op, std::ostream& os) final; //NOLINT(*)
-        void VisitExpr_(const FloatImm * op, std::ostream& os) final; //NOLINT(*)
-        void VisitStmt_(const IfThenElse* op) final; //NOLINT(*)
+        void VisitExpr_(const Broadcast * op, std::ostream& os) override; //NOLINT(*)
+        void VisitExpr_(const Call * op, std::ostream& os) override; //NOLINT(*)
+        void VisitExpr_(const Select * op, std::ostream& os) override; //NOLINT(*)
+        void VisitExpr_(const FloatImm * op, std::ostream& os) override; //NOLINT(*)
+        void VisitStmt_(const IfThenElse* op) override; //NOLINT(*)
+        void VisitStmt_(const LetStmt* op) override; // NOLINT(*)
         void GenForStmt(const For* op, std::string pragma, bool before);
-        void VisitStmt_(const LetStmt* op) final; // NOLINT(*)
+
 
 
     private:
@@ -57,4 +58,4 @@ class CodeGenOpenCL final : public CodeGenC {
 } // namespace codegen
 } // namespace TVM
 
-#endif // TVM_CODEGEN_CODEGEN_SDACCEL_H_
+#endif // TVM_CODEGEN_CODEGEN_OPENCL_H_
