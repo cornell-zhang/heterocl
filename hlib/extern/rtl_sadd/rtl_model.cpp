d756f4b8772fadbea472f7a5b63d25df31487ebe
Hecmay <sx233@cornell.edu>
2020-04-30 22:57:52 -0400

    clean up

--- hlib/extern/rtl_sadd/rtl_model.cpp
+++
@@ -1,8 +0,0 @@
-// 67d7842dbbe25473c3c32b93c0da8047785f30d78e8a024de1b57352245f9689
-//--------------------------------------------------------
-#include "ap_int.h"
-void rtl_model(ap_int<32> a1, ap_int<32> b1, ap_int<32> &z1)
-{
-#pragma HLS inline off
-  z1=a1+b1;
-}
--- hlib/tests/test_extern_ip.py
+++ hlib/tests/test_extern_ip.py
@@ -7,64 +7,6 @@ from itertools import permutations
 
 dtype = hcl.Float(64)
 
-def test_vector_add():
-
-    def _test_llvm(in_shape):
-        hcl.init(hcl.Float())
-        A = hcl.placeholder(in_shape, name="A")
-        B = hcl.placeholder(in_shape, name="B")
-
-        def math_func(A, B):
-            return hlib.op.extern.vector_add_rtl(A, B)
-
-        s = hcl.create_schedule([A, B], math_func)
-        f = hcl.build(s)
-
-        _a = 10 * np.random.random(in_shape) - 5
-        _b = 10 * np.random.random(in_shape) - 5
-        out = hcl.asarray(np.zeros(in_shape).astype('float32'))
-
-        real_out = np.add(_a, _b)
-        f(hcl.asarray(_a), hcl.asarray(_b), out)
-        tst.assert_almost_equal(out.asnumpy(), real_out, 4)
-
-    _test_llvm((1, 3))
-    _test_llvm((3, 3, 3))
-    _test_llvm((5, 5, 3, 2))
-
-    if (os.system("which v++ >> /dev/null") != 0):
-      return 
-  
-    def _test_sim(in_shape):
-
-        hcl.init(hcl.Int())
-        A = hcl.placeholder(in_shape, name="A")
-        B = hcl.placeholder(in_shape, name="B")
-
-        def kernel(A, B):
-            C = hlib.op.extern.scalar_add_rtl(A, B, name="C")
-            return hcl.compute(in_shape, lambda *args: C[args] * 2, "D")
-
-        target = hcl.platform.aws_f1
-        target.config(compile="vitis", mode="sw_sim", backend="vhls")
-        s = hcl.create_schedule([A, B], kernel)
-
-        s.to([A, B], target.xcel)
-        s.to(kernel.C, target.host)
-
-        f = hcl.build(s, target)
-        _a = 10 * np.random.random(in_shape) - 5
-        _b = 10 * np.random.random(in_shape) - 5
-        out = hcl.asarray(np.zeros(in_shape).astype('int32'))
-
-        real_out = np.add(_a, _b) * 2
-        f(hcl.asarray(_a), hcl.asarray(_b), out)
-        tst.assert_almost_equal(out.asnumpy(), real_out, 4)
-
-    _test_sim((1, 3))
-    _test_sim((3, 3, 3))
-    _test_sim((5, 5, 3, 2))
-
 def test_fft_hls():
 
     def _test_llvm(length):
