89c4db3b1f885d48e3f119ed61d73d2cee7b4a93
Hecmay <sx233@cornell.edu>
2020-05-28 20:38:33 -0400

    fix sim error

--- samples/optical_flow/optic_flow_single.py
+++ samples/optical_flow/optic_flow_single.py
@@ -2,6 +2,7 @@ import heterocl as hcl
 import numpy as np
 import os, sys
 from PIL import Image
+# np.set_printoptions(threshold=np.inf)
 
 # height x width
 size = (436, 1024)
@@ -22,9 +23,8 @@ def optical_flow(target=target):
     image2 = hcl.placeholder(size, "input_image2")
     image3 = hcl.placeholder(size, "input_image3")
     image4 = hcl.placeholder(size, "input_image4")
-    output = hcl.placeholder((436,1024,2), "output_image")
 
-    def kernel(img0, img1, img2, img3, img4, output):
+    def kernel(img0, img1, img2, img3, img4):
 
        sum = hcl.reducer(0, lambda x, y: x + y, dtype)
 
@@ -36,8 +36,8 @@ def optical_flow(target=target):
            # 1d conv slong x and y axis
            def update(y, x):
                with hcl.if_(hcl.and_(y>=2, y<height-2, x>=2, x<width-2)):
-                   grad_x[y,x] = sum(input_image[y,x+rx-2] * g_w[rx], axis=rx)
-                   grad_y[y,x] = sum(input_image[y+ry-2,x] * g_w[ry], axis=ry)
+                   grad_x[y,x] = sum(input_image[y,x+rx-2] * g_w[rx], axis=rx) / 12.0
+                   grad_y[y,x] = sum(input_image[y+ry-2,x] * g_w[ry], axis=ry) / 12.0
            hcl.mutate(size, lambda y, x: update(y, x))
            
        @hcl.def_([size, size, size, size, size, size])
@@ -51,13 +51,13 @@ def optical_flow(target=target):
                              img4[y,x] * g_w[4]) / 12.0)
 
        # averaging gradients in y dim
-       @hcl.def_([size, size, size, (436,1024,3)])
+       @hcl.def_([size, size, size, (*size,3)])
        def grad_weight_y(grad_x, grad_y, grad_z, y_filt):
            g_f = hcl.copy(grad_filter, "g_f", hcl.Float())
            # rd1 = hcl.reduce_axis(0, 7, name="rdx1")
            # rd2 = hcl.reduce_axis(0, 7, name="rdx2")
            # rd3 = hcl.reduce_axis(0, 7, name="rdx3")
-           def acc(y, x):
+           def update(y, x):
                with hcl.if_(hcl.and_(y>=3, y<height-3)):
                    v1 = hcl.scalar(0, "v1")
                    v2 = hcl.scalar(0, "v2")
@@ -73,7 +73,7 @@ def optical_flow(target=target):
                    #         hcl.select(c==0, grad_x[y+rd-3,x],
                    #         hcl.select(c==1, grad_y[y+rd-3,x],
                    #         grad_z[y+rd-3,x])) * g_f[rd], axis=rd)
-           hcl.mutate(size, acc)
+           hcl.mutate(size, update)
 
        @hcl.def_([(*size,3), (*size,3)])
        def grad_weight_x(y_filt, filt_grad):
@@ -99,12 +99,12 @@ def optical_flow(target=target):
        def outer_product(filt_grad, outer):
            # save the condition logic
            def update(y, x):
-               outer[y,x,0] = filt_grad[0,y,x] * filt_grad[0,y,x]
-               outer[y,x,1] = filt_grad[1,y,x] * filt_grad[1,y,x]
-               outer[y,x,2] = filt_grad[0,y,x] * filt_grad[2,y,x]
-               outer[y,x,3] = filt_grad[0,y,x] * filt_grad[1,y,x]
-               outer[y,x,4] = filt_grad[0,y,x] * filt_grad[2,y,x]
-               outer[y,x,5] = filt_grad[1,y,x] * filt_grad[2,y,x]
+               outer[y,x,0] = filt_grad[y,x,0] * filt_grad[y,x,0]
+               outer[y,x,1] = filt_grad[y,x,1] * filt_grad[y,x,1]
+               outer[y,x,2] = filt_grad[y,x,2] * filt_grad[y,x,2]
+               outer[y,x,3] = filt_grad[y,x,0] * filt_grad[y,x,1]
+               outer[y,x,4] = filt_grad[y,x,0] * filt_grad[y,x,2]
+               outer[y,x,5] = filt_grad[y,x,1] * filt_grad[y,x,2]
            hcl.mutate(size, update)
 
            # hcl.update(outer, 
@@ -193,10 +193,9 @@ def optical_flow(target=target):
        out_product = hcl.compute((*size,6), init, name="out_product")
        tensor_y    = hcl.compute((*size,6), init, name="tensor_y")
        tensor      = hcl.compute((*size,6), init, name="tensor")
+       output      = hcl.compute((*size,2), init, name="output")
 
        calc_xy_gradient(image2, grad_x, grad_y)
-       hcl.print(image2)
-       hcl.print(grad_x)
        calc_z_gradient(image0, image1, image2, image3, image4, grad_z)
 
        grad_weight_y(grad_x, grad_y, grad_z, y_filt)
@@ -206,8 +205,9 @@ def optical_flow(target=target):
        tensor_weight_y(out_product, tensor_y)
        tensor_weight_x(tensor_y, tensor)
        flow_calc(tensor, output)
+       return output
 
-    s = hcl.create_schedule([image0, image1, image2, image3, image4, output], kernel)
+    s = hcl.create_schedule([image0, image1, image2, image3, image4], kernel)
 
     if target != "llvm":
 
@@ -227,7 +227,7 @@ def optical_flow(target=target):
       s.to(kernel.tensor_y, s[k_tensor_x], s[k_tensor_y], hcl.Stream.FIFO)
       s.to(kernel.tensor, s[k_calc_flow], s[k_tensor_x], hcl.Stream.FIFO)
 
-    print(hcl.lower(s))
+    # print(hcl.lower(s))
     return hcl.build(s, target)
 
 # load ppm image amd convert to grayscale
@@ -244,11 +244,48 @@ img3 = np.asarray(img3.getdata(), dtype=np.uint32).reshape(img3.size[1], img3.si
 img4 = np.asarray(img4.getdata(), dtype=np.uint32).reshape(img4.size[1], img4.size[0]) 
 imgs = [img0, img1, img2, img3, img4]
 
-hcl_output = hcl.asarray(np.zeros((463,1024,2)), dtype)    
-hcl_grad_x = hcl.asarray(np.zeros((463,1024,6)), dtype)    
+hcl_output = hcl.asarray(np.zeros((*size,2)), dtype)    
 imgs = [hcl.asarray(_) for _ in imgs]
 
 f = optical_flow(target)
 f(*imgs, hcl_output)
-print(hcl_output.asnumpy())
-print(hcl_grad_x.asnumpy())
+res = hcl_output.asnumpy()
+
+# read ref optic flow vector 
+ref_flo = "datasets/current/ref.flo"
+with open(ref_flo, 'rb') as f:
+    magic = np.fromfile(f, np.float32, count=1)
+    if 202021.25 != magic:
+        raise RuntimeError('Magic number incorrect. Invalid .flo file')
+    else:
+        w = np.fromfile(f, np.int32, count=1)[0]
+        h = np.fromfile(f, np.int32, count=1)[0]
+        print('Reading %d x %d flo file' % (w, h))
+        data = np.fromfile(f, np.float32, count=2*w*h)
+        # Reshape data into 3D array (columns, rows, bands)
+        ref = np.resize(data, (h, w, 2))
+
+# check the error 
+accum_err = 0
+pixel_num = 0
+out = np.zeros((height,width))
+for y in range(height):
+    for x in range(width):
+        out_x, out_y = res[y, x] 
+
+        if out_x**2 + out_y**2 > 25:
+            out_x = 1e10
+            out_y = 1e10
+
+        ref_x, ref_y = ref[y, x]
+        ref_deg = np.math.atan2(-ref_y, -ref_x)  * 180 / np.pi
+        out_deg = np.math.atan2(-out_y, -out_x)  * 180 / np.pi
+
+        err_deg = out_deg - ref_deg
+        while err_deg < -180: err_deg += 360
+        while err_deg > 180: err_deg -= 360
+        accum_err += abs(err_deg)
+        pixel_num += 1
+
+print("Average error: {} degress ".format(accum_err/pixel_num))
+
