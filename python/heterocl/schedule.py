580a3625a3ade2608be9b69272e92e0034f7fc35
Yi-Hsiang (Sean) Lai <seanlatias@users.noreply.github.com>
2020-06-28 21:33:18 -0400

    [API] Fix reuse_at cannot be used on tensor generated by hcl.compute (#255)

--- python/heterocl/schedule.py
+++ python/heterocl/schedule.py
@@ -13,7 +13,7 @@ from .tvm import _api_internal
 from .tvm._api_internal import _ExternOp
 from .debug import DSLError, APIError
 from . import util
-from .devices import Device, DevMediaPair 
+from .devices import Device, DevMediaPair
 
 class Schedule(object):
     """Create a compute schedule.
@@ -115,7 +115,7 @@ class Schedule(object):
                                 channel.op.name, "<==", name)
 
                     # add children nodes to graph
-                    else: 
+                    else:
                         if plot:
                             print(name_with_prefix,  " <=== ", name)
                         graph.add_edge(name, name_with_prefix)
@@ -137,7 +137,7 @@ class Schedule(object):
             pos[stage.name_with_prefix] = (x, 0)
             x += 1
 
-        if plot: # draw the network 
+        if plot: # draw the network
             try:
                 from networkx.drawing.nx_agraph import graphviz_layout
             except ImportError:
@@ -153,12 +153,12 @@ class Schedule(object):
         assert len(inputs) > 0, "empty inputs"
         assert len(outputs) > 0, "empty outputs"
 
-        # check availability 
+        # check availability
         graph, op_map = self.dataflow_graph()
         inputs  = [ _.name for _ in inputs ]
         outputs = [ _.name for _ in outputs ]
 
-        # from root to parents 
+        # from root to parents
         stack = deepcopy(outputs)
         subgraph = list()
         while len(stack) > 0:
@@ -189,7 +189,7 @@ class Schedule(object):
             axis = post_stage.op.axis[axis_num-1]
             pre_stage.compute_at(post_stage, axis)
 
-        # split kernel 
+        # split kernel
         post_stage.split(post_stage.op.axis[0], factor)
         return post_stage
 
@@ -221,7 +221,7 @@ class Schedule(object):
         """
         try:
             target = target.tensor
-        except AttributeError:
+        except (AttributeError, ValueError):
             try:
                 target = target._op
             except AttributeError:
@@ -246,11 +246,11 @@ class Schedule(object):
                 target = dest._op
             elif isinstance(dest, tuple):
                 src, target = dest
-            else: # target tensor 
+            else: # target tensor
                 target = dest.tensor
         else: target = dest
 
-        for src in srcs: 
+        for src in srcs:
             if isinstance(src, tuple):
                 src, tensor = src
                 assert tensor == target, + \
@@ -269,8 +269,8 @@ class Schedule(object):
 
     def to(self, tensors, dst, src=None, axis=0,
            stream_type=_expr.Stream.Copy, depth=1, name=None):
-        """Stream a list of Tensors to dst devices 
-        
+        """Stream a list of Tensors to dst devices
+
         Parameters
         ----------
         tensors : list of Tensor
@@ -302,12 +302,12 @@ class Schedule(object):
             try:
                 if isinstance(tensor, Stage):
                     target = tensor._op
-                # unpack tuple of src stage and tensor 
+                # unpack tuple of src stage and tensor
                 elif isinstance(tensor, tuple):
                     src, target = tensor
                     # from hcl stage to tvm stage
                     src = self.__getitem__(src)
-                else: # target tensor 
+                else: # target tensor
                     target = tensor.tensor
             except (AttributeError, ValueError):
                 target = tensor
@@ -321,9 +321,9 @@ class Schedule(object):
                 # move to device
                 if isinstance(dst, Device) or \
                         isinstance(dst, DevMediaPair):
-                    if axis == 0: 
+                    if axis == 0:
                         move_to_device = True
-                    else: # inner-stage movement  
+                    else: # inner-stage movement
                         assert isinstance(tensor, Stage)
                         target = self[tensor]
 
@@ -333,7 +333,7 @@ class Schedule(object):
             # target can be stage or tensor
             ret = self.sch.to(target, dst, src, axis,
                               stream_type, depth)
-            # record the placement information 
+            # record the placement information
             if move_to_device:
                 channel, ret = ret
                 self.placement[target.name] = \
@@ -568,7 +568,7 @@ class Stage(object):
                 for stage in self.input_stages:
                     if stage.name == name:
                         return (self, stage._op)
-                # check tensors in input_stage.lhs 
+                # check tensors in input_stage.lhs
                 for stage in self.input_stages:
                     lhs = stage.lhs_tensors
                     for tensor in lhs:
