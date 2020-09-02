85a0366b78f10a09c6902496f6499a858858a125
Hecmay <sx233@cornell.edu>
2020-09-01 22:19:03 -0400

    systolic array update

--- samples/systolic_array/systolic_array_module.py
+++ samples/systolic_array/systolic_array_module.py
@@ -12,46 +12,77 @@ def systolic(m=2, k=2, n=2, w=2, h=2, dtype=hcl.Int(), target=None):
     def systolic_array(A, B):
 
         # define modules with new decorator
-        @hcl.def2_([(1,), (1,), (3,)], amount=w*h)
-        def pe(a, b, out):
-            out[0] = a
-            out[1] = b
-            out[2] = a * b
+        @hcl.def2_([(1,), (1,), (1,), (1,), (1,)], amount=w*h)
+        def pe(a, b, out_a, out_b, out):
+            out_a.v = a
+            out_b.v = b
+            out.v = a * b
 
         # each k calls of update function calculate one block of result matrix
         # b_row: block row index
         # b_col: block col index
-        def update(b_row, b_col, k, O):
-            # fetch input
-            localA = []
-            localB = []
-            for input_a in range(h):
-                localA.append(hcl.compute((1,), lambda x : A[input_a + h * b_row, k], "localA_{}".format(input_a)))
-            for input_b in range(w):
-                localB.append(hcl.compute((1,), lambda x : B[k, input_b + w * b_col], "localB_{}".format(input_b)))
-
-            # systolic connection
-            net = [[None] * h] * w
-            for i in range(h + w - 1):
-                for row in range(i + 1):
-                    col = i - row
-                    if col < 0 or col > w-1 or row > h-1: continue
-                    ## instantiate a PE and record partial results
-                    input_a = localA[row] if col == 0 else hcl.compute((1,), lambda x : net[row][col-1][0], "input_a{}{}".format(row, col))
-                    input_b = localB[col] if row == 0 else hcl.compute((1,), lambda x : net[row-1][col][1], "input_b{}{}".format(row, col))
-
-                    out = hcl.compute((3,), lambda x : 0, "out_{}{}".format(row, col))
-                    pe[row * w + col](input_a, input_b, out)
-                    hcl.print((out[2], input_a, input_b), "out=%d a=%d b=%d\n")
-
-                    # MAC adds up the intermediate result
-                    O[row + h * b_row, col + w * b_col] += out[2]
-                    net[row][col] = out
 
         block_rows = int(m / h)
         block_cols = int(n / w)
         O = hcl.compute((m, n), lambda *args : 0, name="Output")
-        hcl.mutate((block_rows, block_cols, k), lambda b_row, b_col, k: update(b_row, b_col, k, O), name="update")
+
+        with hcl.Stage("update") as s:
+            with hcl.for_(0, block_rows, name="row") as b_row:
+                with hcl.for_(0, block_cols, name="col") as b_col:
+
+                    # stage 1: create pipes
+                    localA = []
+                    localB = []
+                    p_out = hcl.compute((h,w), lambda y, x: 0, "localO")
+                    for input_a in range(h):
+                        localA.append(hcl.compute((k,), lambda x: 0, "localA_{}".format(input_a)))
+                    for input_b in range(w):
+                        localB.append(hcl.compute((k,), lambda x: 0, "localB_{}".format(input_b)))
+
+                    with hcl.for_(0, k, name="_k") as _k:
+                        for input_a in range(h):
+                            localA[input_a][_k] = A[input_a + h * b_row, _k] 
+                        for input_b in range(w):
+                            localB[input_b][_k] = B[_k, input_b + w * b_col] 
+
+                    # stage 2: do computation
+                    with hcl.for_(0, k, name="k") as k_:
+                        # systolic connection
+                        net_a = [[None] * h] * w
+                        net_b = [[None] * h] * w
+                        # save the partial sum into local buffer 
+                        for i in range(h + w - 1):
+                            for row in range(i + 1):
+                                col = i - row
+                                if col < 0 or col > w-1 or row > h-1: continue
+                                ## instantiate a PE and record partial results
+                                input_a = hcl.scalar(0, "input_a{}{}".format(row, col))
+                                input_b = hcl.scalar(0, "input_b{}{}".format(row, col))
+                                if col == 0:
+                                    input_a.v = localA[row][k_] 
+                                else:
+                                    input_a.v = net_a[row][col-1][0]
+                                if row == 0:
+                                    input_b.v = localB[col][k_]
+                                else:
+                                    input_b.v = net_b[row-1][col][0]
+
+                                out   = hcl.scalar(0, "out_{}{}".format(row, col))
+                                out_a = hcl.scalar(0, "out_a_{}{}".format(row, col))
+                                out_b = hcl.scalar(0, "out_b_{}{}".format(row, col))
+                                pe[row * w + col](input_a, input_b, out_a, out_b, out)
+                                # hcl.print((out[2], input_a, input_b), "out=%d a=%d b=%d\n")
+
+                                # MAC adds up the intermediate result
+                                p_out[row][col] += out
+                                net_a[row][col] = out_a
+                                net_b[row][col] = out_b
+
+                    # stage 3: push results to output
+                    with hcl.for_(0, h, name="h_") as h_:
+                        with hcl.for_(0, w, name="w_") as w_:
+                            O[h_ + h * b_row, w_ + w * b_col] = p_out[h_, w_]
+            
         return O
 
     s = hcl.create_schedule([Ain, Bin], systolic_array)
@@ -69,7 +100,7 @@ def systolic(m=2, k=2, n=2, w=2, h=2, dtype=hcl.Int(), target=None):
     if isinstance(target, hcl.platform):
         s.to([Ain, Bin], target.xcel)
         s.to(systolic_array.update.Output, target.host)
-        target.config(compile="vivado_hls", mode="csyn")
+        target.config(compile="vitis", mode="hw_exe")
 
     print(hcl.lower(s))
     return hcl.build(s, target=target)
@@ -77,12 +108,12 @@ def systolic(m=2, k=2, n=2, w=2, h=2, dtype=hcl.Int(), target=None):
 
 # matrix size
 # m*k k*n
-m = 4
-k = 4
-n = 4
+m = 32
+k = 32
+n = 32
 # systolic size
-w = 2
-h = 2
+w = 4
+h = 4
 
 np_1 = np.random.randint(10, size=(m,k))
 np_2 = np.random.randint(10, size=(k,n))
@@ -94,9 +125,9 @@ hcl_m2 = hcl.asarray(np_2, dtype=dtype)
 hcl_m3 = hcl.asarray(np.zeros((m,n)), dtype=dtype)
 
 # systolic array
-# target = hcl.platform.zc706
+target = hcl.platform.zc706
 # target = "vhls_csim"
-target = "llvm"
+# target = "llvm"
 # target = "vhls"
 fs = systolic(m, k, n, w, h, dtype=hcl.Int(), target=target)
 if isinstance(target, str) and target == "vhls":
