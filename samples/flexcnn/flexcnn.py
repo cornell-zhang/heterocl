94867b2231ff421bba8f536663d942a2138ac054
Niansong Zhang <zhangns@mail2.sysu.edu.cn>
2020-11-07 22:32:50 +0800

    [flexcnn] relu dynamic tiling & SIMD (WIP)

--- samples/flexcnn/flexcnn.py
+++ samples/flexcnn/flexcnn.py
@@ -52,6 +52,9 @@ InterWriteData0Type = hcl.UInt(DATA_W0 * INTER_WRITE_LANE)
 InterWriteData1Type = hcl.UInt(DATA_W0 * INTER_WRITE_LANE)
 UpsampleData0Type = hcl.UInt(DATA_W0 * UPSAMPLE_LANE)
 ConfigInst = hcl.UInt(192)
+# Buffer sizes
+OUT_NUM_T = 64 # max out_num_t
+
 
 def top_module(global_cin, global_prev_cin, global_weight, global_bias, global_cout, config):
     """
@@ -184,25 +187,24 @@ def top_module(global_cin, global_prev_cin, global_weight, global_bias, global_c
         # decode inst3
         en = hcl.compute((1,), lambda _ : inst3[0], dtype=hcl.UInt(32))
         layer_en = hcl.unpack(en, dtype=hcl.UInt(1))
-        depth_conv_en = hcl.compute((1,), lambda _ : layer_en[1]) 
-        in_num_t      = hcl.compute((1,), lambda _ : inst3[2], dtype=hcl.UInt(32)) 
-        out_num_t     = hcl.compute((1,), lambda _ : inst3[3], dtype=hcl.UInt(32)) 
-        int_h_t       = hcl.compute((1,), lambda _ : inst3[4], dtype=hcl.UInt(32))
-        int_w_t       = hcl.compute((1,), lambda _ : inst3[5], dtype=hcl.UInt(32))
+        depth_conv_en = hcl.compute((1,), lambda _ : layer_en[1])
+        in_out_num_t        = hcl.unpack(inst3[2], dtype=hcl.UInt(16)) 
+        in_num_t      = hcl.compute((1,), lambda _ : in_out_num_t[0], dtype=hcl.UInt(16)) 
+        out_num_t     = hcl.compute((1,), lambda _ : in_out_num_t[1], dtype=hcl.UInt(16)) 
+        in_h_t       = hcl.compute((1,), lambda _ : inst3[3], dtype=hcl.UInt(32))
+        in_w_t       = hcl.compute((1,), lambda _ : inst3[4], dtype=hcl.UInt(32))
 
         # fetch data form input buffer
-        # dimensions: layer_batch, tile_c, tile_h, tile_w, c, h, w
+        # dimensions: layer_batch, tile_c, tile_h, tile_w, c, h, w, SIMD
         # note: we must use loop to index the elements of cin
         # we can't build a new multi-dimensional Tensor because there's no dynamic shape Tensor.
-        # with hcl.for_():
+        with hcl.for_():
+            pass
 
 
         
         with hcl.if_(depth_conv_en):
             with hcl.if_(filter_s1): 
-                # kernel size should be 1x1
-                # TODO: why padding=[0,0] failed?
-                # this stride is not correct 
                 s = hcl.cast(hcl.Int(32), stride)
                 cout = conv2d(cin, weight, padding=[1,1], groups=1, strides=[s,s], data_layout='NHWC')
             with hcl.else_():
@@ -233,7 +235,7 @@ def top_module(global_cin, global_prev_cin, global_weight, global_bias, global_c
             cout = hcl.compute(cin.shape, lambda *args : cin[args])
 
     @def_([(1,26,26,3), (32,), (1,26,26,3), (32,), (1,), (1,)], dtypes=[DepthConvData0Type, ConfigInst, ReluData0Type, ConfigInst, CinLoadData0Type, CinLoadData0Type], name="relu_bn")
-    def relu(cin, config_in, cout, config_out, gamma_conv, beta_conv):
+    def relu_old(cin, config_in, cout, config_out, gamma_conv, beta_conv):
         # parse layer_config
         en = hcl.compute((1,), lambda _ : config_in[19], dtype=hcl.UInt(32))
         layer_en = hcl.unpack(en, dtype=hcl.UInt(1))
@@ -259,6 +261,111 @@ def top_module(global_cin, global_prev_cin, global_weight, global_bias, global_c
         with hcl.else_():
             cout = hcl.compute(cin.shape, lambda *args : cin[args])
 
+    # note: gamma and beta has the same size with in_num
+    @def_([(10000,), (5,), (10000,), (5,), (1000,), (1000,)], dtypes=[DepthConvData0Type, ConfigInst, ReluData0Type, ConfigInst, CinLoadData0Type, CinLoadData0Type], name="relu_bn")
+    def relu(cin, config_in, cout, config_out, gamma_conv, beta_conv):
+        """
+            ReLU Module: bias + batch norm + relu/relu6
+            cin: input data
+            config_in: input instruction 192-bit x5
+            config_out: output instruction passed to downstream
+            gamma_conv: multiplication parameter, if there is no batchnorm then it's normal bias: gamma=1
+            beta_conv: addition parameter, if there is no batchnorm then it's normal bias: beta = bias
+        """
+        # write config_out
+        config_out = hcl.compute((1,), lambda *args : config_in[args])
+        # parse layer_config
+        inst0 = hcl.unpack(config_in[0], dtype=hcl.UInt(32))
+        inst1 = hcl.unpack(config_in[1], dtype=hcl.UInt(32))
+        inst2 = hcl.unpack(config_in[2], dtype=hcl.UInt(32))
+        inst3 = hcl.unpack(config_in[3], dtype=hcl.UInt(32))
+        inst4 = hcl.unpack(config_in[4], dtype=hcl.UInt(32))
+        # decode inst1
+        in_num_hw  = hcl.compute((1,), lambda _ : inst0[0], dtype=hcl.UInt(32))
+        out_num_hw = hcl.compute((1,), lambda _ : inst0[1], dtype=hcl.UInt(32))
+        in_h_hw    = hcl.compute((1,), lambda _ : inst0[2], dtype=hcl.UInt(32))
+        in_w_hw    = hcl.compute((1,), lambda _ : inst0[3], dtype=hcl.UInt(32))
+        out_h_hw   = hcl.compute((1,), lambda _ : inst0[4], dtype=hcl.UInt(32))
+        out_w_hw   = hcl.compute((1,), lambda _ : inst0[5], dtype=hcl.UInt(32))
+        # decode inst1
+        in_num  = hcl.compute((1,), lambda _ : inst1[0], dtype=hcl.UInt(32))
+        out_num = hcl.compute((1,), lambda _ : inst1[1], dtype=hcl.UInt(32))
+        in_h    = hcl.compute((1,), lambda _ : inst1[2], dtype=hcl.UInt(32))
+        in_w    = hcl.compute((1,), lambda _ : inst1[3], dtype=hcl.UInt(32))
+        out_h   = hcl.compute((1,), lambda _ : inst1[4], dtype=hcl.UInt(32))
+        out_w   = hcl.compute((1,), lambda _ : inst1[5], dtype=hcl.UInt(32))
+        # decode inst2
+        filter_s     = hcl.compute((1,), lambda _ : inst2[4], dtype=hcl.UInt(32))
+        filter_s1_s2 = hcl.unpack(filter_s, dtype=hcl.UInt(16))
+        filter_s1    = hcl.compute((1,), lambda _ : filter_s1_s2[0], "FILTER_S1")
+        stride       = hcl.compute((1,), lambda _ : inst2[5], dtype=hcl.UInt(32))
+        # decode inst3
+        en = hcl.compute((1,), lambda _ : inst3[0], dtype=hcl.UInt(32))
+        layer_en = hcl.unpack(en, dtype=hcl.UInt(1))
+        CONV_EN             = hcl.compute((1,), lambda _ : layer_en[2])
+        RELU_EN             = hcl.compute((1,), lambda _ : layer_en[3], dtype=hcl.UInt(1))
+        RELU6_EN            = hcl.compute((1,), lambda _ : layer_en[4], dtype=hcl.UInt(1))
+        BIAS_EN             = hcl.compute((1,), lambda _ : layer_en[7], dtype=hcl.UInt(1))
+        BATCH_NORM_EN       = hcl.compute((1,), lambda _ : layer_en[10], dtype=hcl.UInt(1))
+        BATCH_NORM_EN_DEPTH = hcl.compute((1,), lambda _ : layer_en[12], dtype=hcl.UInt(1))
+        in_out_num_t        = hcl.unpack(inst3[2], dtype=hcl.UInt(16))
+        in_num_t            = hcl.compute((1,), lambda _ : in_out_num_t[0], dtype=hcl.UInt(16)) 
+        out_num_t           = hcl.compute((1,), lambda _ : in_out_num_t[1], dtype=hcl.UInt(16)) 
+        in_h_t             = hcl.compute((1,), lambda _ : inst3[3], dtype=hcl.UInt(32))
+        in_w_t             = hcl.compute((1,), lambda _ : inst3[4], dtype=hcl.UInt(32))
+
+        # define beta and gamma buffers for SIMD
+        beta_buf  = hcl.compute((OUT_NUM_T / RELU_LANE, RELU_LANE), lambda _ : 0, dtype=data_t2, name="beta_buffer")
+        gamma_buf = hcl.compute((OUT_NUM_T / RELU_LANE, RELU_LANE), lambda _ : 0, dtype=data_t2, name="gamma_buffer")
+        cin_buf   = hcl.compute((RELU_LANE,), lambda _ : 0, dtype=data_t0, name="cin_buffer")
+        cout_buf  = hcl.compute((RELU_LANE,), lambda _ : 0, dtype=hcl.UInt(DATA_W0), name="cout_buffer")
+
+        # branch conditions
+        cond    = hcl.or_(RELU_EN, RELU6_EN, BIAS_EN, BATCH_NORM_EN)
+        bias_en = hcl.and_(BIAS_EN, CONV_EN)
+
+        with hcl.if_(cond):
+            # branch: bias or batch norm
+            # note: we only deal with batch=1 (LAYER_BATCH)
+            with hcl.for_(0, out_num, out_num_t) as out_num_iter:
+                with hcl.for_(0, in_w, in_w_t)     as in_w_iter:
+                    with hcl.for_(0, in_h, in_h_t)   as in_h_iter:
+                        with hcl.for_(0, in_num, in_num_t) as in_num_iter:
+                            """ inside each tile """
+                            # fetch beta and gamma
+                            with hcl.for_(0, out_num_t / RELU_LANE) as o:
+                                # TODO: careful with beta and gamma sizes!
+                                beta_wide  = hcl.unpack(beta_conv[o], dtype=hcl.UInt(32))
+                                gamma_wide = hcl.unpack(gamma_conv[o], dtype=hcl.UInt(32))
+                                with hcl.for_(0, RELU_LANE) as lane:
+                                    beta_buf[(o, lane)] = hcl.cast(hcl.Float(32), beta_wide[lane])# TODO: is this indexing correct?
+                                    gamma_buf[(o, lane)] = hcl.cast(hcl.Float(32), gamma_wide[lane])
+                            # perform calculation
+                            with hcl.for_(0, out_num_t / RELU_LANE) as c:
+                                with hcl.for_(0, in_h_t / stride)     as h:
+                                    with hcl.for_(0, in_w_t / stride)   as w:
+                                        # unpack data
+                                        # TODO: find out how to calculate this index
+                                        idx = 0 # place holder
+                                        unpacked_cin = hcl.unpack(cin[idx], hcl.UInt(32))
+                                        cin_float = hcl.cast(hcl.Float(32), unpacked_cin)
+                                        # bias/batchnorm
+                                        with hcl.if_(hcl.or_(bias_en, BATCH_NORM_EN)):
+                                            cout_packed = hcl.compute((RELU_LANE,), lambda *args: cin_float[args] * gamma_buf[(c, args)])
+                                            cout[idx] = cout_packed
+                                        # relu
+                                        with hcl.if_(hcl.and_(RELU6_EN, BATCH_NORM_EN_DEPTH == 0)):
+                                            tmp = hcl.compute((RELU_LANE,), lambda *args : hcl.select(cin_float[args] < 0, 0, cin_float[args]))
+                                            cout_packed = hcl.compute((RELU_LANE,), lambda *args : hcl.select(tmp[args] > 6, 6, tmp[args]))
+                                            cout[idx] = cout_packed
+                                        with hcl.elif_(RELU_EN):
+                                            cout_packed = hcl.compute((RELU_LANE,), lambda *args : hcl.select(cin_float[args] > 6, 6, tmp[args]))
+                                            cout[idx] = cout_packed 
+                                        
+        with hcl.else_():
+            # bypass
+            cout = hcl.compute(cin.shape, lambda *args : cin[args])
+            
 
 
     # not available in hlib.nn
@@ -276,7 +383,6 @@ def top_module(global_cin, global_prev_cin, global_weight, global_bias, global_c
 
         with hcl.if_(UP_SAMPLE_EN):
             n, h, w, c = cin.shape
-            # TODO: is this ok?
             cout = hcl.compute((n, h*2, w*2, c), lambda n_i,h_i,w_i,c_i : cin[n_i][h_i/2][w_i/2][c_i])
         with hcl.else_():
             cout = hcl.compute(cin.shape, lambda *args : cin[args]) 
