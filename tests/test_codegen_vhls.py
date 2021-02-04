d9b22a86d7b75fc9c46253f38ae0f37fadd12376
seanlatias <seanlatias@gmail.com>
2021-02-04 09:13:40 -0500

    add test

--- tests/test_codegen_vhls.py
+++ tests/test_codegen_vhls.py
@@ -26,6 +26,15 @@ def test_dtype():
     test_ap_int()
     test_ap_fixed()
 
+def test_print():
+    hcl.init()
+    A = hcl.placeholder((10, 32))
+    def kernel(A):
+        hcl.print(A[0])
+        return hcl.compute(A.shape, lambda *args: A[args])
+    s = hcl.create_schedule([A], kernel)
+    code = hcl.build(s, target='vhls')
+
 def test_pragma():
     hcl.init()
     A = hcl.placeholder((10, 32), "A")
@@ -139,7 +148,7 @@ def test_select_type_cast():
     def test_imm_ops():
         A = hcl.placeholder((10, 10), "A")
         def kernel(A):
-            return hcl.compute((8, 8), lambda y, x: 
+            return hcl.compute((8, 8), lambda y, x:
                 hcl.select(x < 4, A[y][x] + A[y+2][x+2], 0), "B")
         s = hcl.create_scheme(A, kernel)
         s = hcl.create_schedule_from_scheme(s)
@@ -150,7 +159,7 @@ def test_select_type_cast():
     def test_uint_imm_ops():
         A = hcl.placeholder((10, 10), "A", dtype=hcl.UInt(1))
         def kernel(A):
-            return hcl.compute((8, 8), lambda y, x: 
+            return hcl.compute((8, 8), lambda y, x:
                 hcl.select(x < 4, A[y][x], 0), "B")
         s = hcl.create_scheme(A, kernel)
         s = hcl.create_schedule_from_scheme(s)
@@ -161,7 +170,7 @@ def test_select_type_cast():
         A = hcl.placeholder((8, 8), "A", dtype=hcl.Int(20))
         B = hcl.placeholder((8, 8), "B", dtype=hcl.Fixed(16,12))
         def kernel(A, B):
-            return hcl.compute((8, 8), lambda y, x: 
+            return hcl.compute((8, 8), lambda y, x:
                 hcl.select(x < 4, A[y][x], B[y][x]), "C", dtype=hcl.Int(8))
         s = hcl.create_scheme([A, B], kernel)
         s = hcl.create_schedule_from_scheme(s)
@@ -172,7 +181,7 @@ def test_select_type_cast():
         A = hcl.placeholder((8, 8), "A", dtype=hcl.Fixed(20,12))
         B = hcl.placeholder((8, 8), "B", dtype=hcl.UFixed(16,12))
         def kernel(A, B):
-            return hcl.compute((8, 8), lambda y, x: 
+            return hcl.compute((8, 8), lambda y, x:
                 hcl.select(x < 4, A[y][x], B[y][x]), "C", dtype=hcl.Int(8))
         s = hcl.create_scheme([A, B], kernel)
         s = hcl.create_schedule_from_scheme(s)
