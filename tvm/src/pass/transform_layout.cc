65eff7b538c432c8c2350a9a205db3ea67541366
Your Name <you@example.com>
2021-02-16 16:25:31 +0000

    update TaskNode to represent the graph node

--- tvm/src/pass/transform_layout.cc
+++ tvm/src/pass/transform_layout.cc
@@ -15,8 +15,54 @@ namespace TVM {
 namespace ir {
 
 using std::unordered_map;
+using std::unordered_set;
 using std::vector;
 using std::string;
+
+struct TaskNode {
+  string name;
+  // Tensor being updated in the task
+  unordered_set<const Variable*> updated_tensors;
+  // Input tensors needed by the task
+  unordered_set<const Variable*> input_tensors;
+  // Children tasks name
+  unordered_set<string> children;
+  // Parent tasks name
+  unordered_set<string> parents;
+};
+
+// Check all the tensors in the Stmt. Get information
+// of their access pattern (write_only, read_only or read_write)
+class BufferStatusCollector : public ir::IRMutator {
+ public:
+  Stmt Mutate_(const Store* op, const Stmt& s) {
+    Stmt stmt = IRMutator::Mutate_(op, s);
+    op = stmt.as<Store>();
+    if (!local_buffers.count(op->buffer_var.get())) {
+      updated_tensors.insert(op->buffer_var.get());
+    }
+    return stmt;
+  }
+
+  Expr Mutate_(const Load* op, const Expr& e) {
+    auto name = op->buffer_var.get()->name_hint;
+    if (!local_buffers.count(op->buffer_var.get())) {
+      input_tensors.insert(op->buffer_var.get());
+    }
+    return IRMutator::Mutate_(op, e);
+  }  
+
+  Stmt Mutate_(const Allocate* op, const Stmt& s) {
+    local_buffers.insert(op->buffer_var.get());
+    Stmt stmt = IRMutator::Mutate_(op, s);
+    op = stmt.as<Allocate>();
+    return stmt;
+  }
+
+  unordered_set<const Variable*> local_buffers;
+  unordered_set<const Variable*> updated_tensors;
+  unordered_set<const Variable*> input_tensors;
+};
 
 // Each task in the graph represents the logic performed by 
 // a HCL stage. The task graph is a coarse grained DFG. 
@@ -50,8 +96,13 @@ class TaskGraphBuilder : public IRMutator {
       Stmt body = this->Mutate(op->body);
       if (op->is_producer) {
         std::string name = op->func->func_name();
-        task_name_to_stmt[name] = s;
-        HCL_DEBUG_LEVEL(2) << "[ debug ] producing tensor " << name;
+
+        // Create a task node in the graph
+        BufferStatusCollector bsc;
+        bsc.Mutate(op->body);
+        TaskNode task = {
+          name, bsc.updated_tensors, bsc.input_tensors, {}, {}
+        };
       
         // Checking depending input tensors
         Array<Var> kernel_input_vars;
@@ -60,11 +111,21 @@ class TaskGraphBuilder : public IRMutator {
           kernel_input_vars.push_back(v);
         }
         Array<Var> undefs = UndefinedVars(body, kernel_input_vars);
+        // The task can be a producer of a tensor, or it will just update
+        // a set of tensors. If the input tensor is not defined in this task 
+        // nor in the input arguments, then it must have been defined in the 
+        // previous tasks visited in the traversal
         for (auto& var: undefs) {
-          string tensor_name = var.get()->name_hint;
-          CHECK(task_name_to_stmt.count(tensor_name)) 
-            << "Cannot locate producer of tensor " << tensor_name;
+          auto parents = checkTensorLiveness(var.get());
+          for (auto& parent_task_name: parents) {
+            task.parents.insert(parent_task_name);
+            CHECK(task_map.count(parent_task_name));
+            task_map[parent_task_name].children.insert(name);
+          }
+
         }
+        task_map[name] = task;
+        HCL_DEBUG_LEVEL(2) << "[ debug ] producing tensor " << name;
       }
       top_level_producer_ = true;
       return ProducerConsumer::make(op->func, op->is_producer, body);
@@ -72,31 +133,67 @@ class TaskGraphBuilder : public IRMutator {
     return IRMutator::Mutate_(op, s);
   }
 
+  // Return the nearest parent task that a tensor has been updated
+  vector<string> checkTensorLiveness(const Variable* var) {
+      // Tasks where the tensor has been updated
+      vector<string> parents;
+      for (auto& kv: task_map) {
+        for (auto& t: kv.second.updated_tensors) {
+          if (t == var) {
+            HCL_DEBUG_LEVEL(2) << "[ debug ] Tensor " << var->name_hint 
+              << " has been updated in task " << kv.second.name;
+            parents.push_back(kv.second.name);
+          }
+        }
+      }
+      return parents;
+  };
+
   // Check the task graph inside the device scope
   bool device_scope_{false};
   bool top_level_producer_{true};
   // Input tensor to the top level function
   Array<VarExpr> kernel_input_args;
-  unordered_map<string, Stmt> task_name_to_stmt;
-  unordered_map<string, vector<string> > dep_task_map;
+  // Map from task name to TaskNode
+  unordered_map<string, TaskNode> task_map;
+
 };
 
+// 1. Locate the which tensor (in which stage) will be layout transformed
+// 2. Locate its parent task and insert the layout mutation statements 
 class LayoutTransformer : public IRMutator {
  public:
   explicit LayoutTransformer(
-    unordered_map<string, vector<string> >& dep_task_map)
-    : dep_task_map_(dep_task_map) { }
-  unordered_map<string, vector<string> >& dep_task_map_;
+    unordered_map<string, TaskNode>& task_map)
+    : task_map_(task_map) { }
+  unordered_map<string, TaskNode>& task_map_;
+
+  Stmt Mutate_(const AttrStmt* op, const Stmt& s) {
+
+    // The tensor to be transformed
+    if (op->attr_key == attr::tensor_layout_attrs) {
+
+        VarExpr var(op->node.node_);
+        auto name = var.get()->name_hint;
+        HCL_DEBUG_LEVEL(2) << "[ debug ] Transform layout of tensor " 
+          << name << "..." << op->body;
+
+        return this->Mutate(op->body);
+    }
+    return IRMutator::Mutate_(op, s);
+  }
+
 };
 
 Stmt TransformLayout(Stmt stmt, Array<NodeRef> api_args) {
   // Restore the task graph from the IR
+  HCL_DEBUG_LEVEL(2) << "------------ Transform Layout --------------";
   TaskGraphBuilder tgb(api_args);
   stmt = tgb.Mutate(stmt);
 
   // Check the tensor in worklist (to be transposed or packed)
-  // Here we create a new attribute statment key "tensor_view"
-  LayoutTransformer ltm(tgb.dep_task_map);
+  // Here we create a new attribute statment key "tensor_view
+  LayoutTransformer ltm(tgb.task_map);
   stmt = ltm.Mutate(stmt);
   return stmt;
 }
