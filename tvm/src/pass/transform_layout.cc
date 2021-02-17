747704b2940907615b1622a2fa02f0bfe88e5b73
Your Name <you@example.com>
2021-02-17 00:47:31 +0000

    add loop axis reorder

--- tvm/src/pass/transform_layout.cc
+++ tvm/src/pass/transform_layout.cc
@@ -6,6 +6,7 @@
 #include <tvm/ir.h>
 #include <tvm/ir_pass.h>
 #include <tvm/ir_mutator.h>
+#include <tvm/ir_visitor.h>
 #include <unordered_set>
 #include <unordered_map>
 #include <vector>
@@ -29,6 +30,111 @@ struct TaskNode {
   unordered_set<string> children;
   // Parent tasks name
   unordered_set<string> parents;
+};
+
+// TODO: save common passes into ir_pass.h
+class IterRangeCollector final : public IRVisitor {
+  public:
+    IterRangeCollector(
+      std::unordered_map<const Variable*, Expr>& range, 
+      std::vector<const Variable*>& loop_iter_vars)
+      : range_(range), loop_iter_vars_(loop_iter_vars) {}
+    void Visit_(const For* op) override {
+      range_[op->loop_var.get()] = Simplify(op->extent - 1);
+      loop_iter_vars_.push_back(op->loop_var.get());
+      this->Visit(op->body);
+    }
+    std::unordered_map<const Variable*, Expr>& range_;
+    std::vector<const Variable*>& loop_iter_vars_;
+};
+
+// Reorder the loop iterator in the buffer index
+// Example: if the tensor B is accessed (with in the loop nest)
+//     B[a1*factor + a2]
+// Then after mutation, we got B[a2*factor+a1]
+Stmt ReorderBufferAccess(Stmt s, string tensor_name) {
+  std::unordered_map<const Variable*, Expr> range_;
+  std::vector<const Variable*> loop_iter_vars_;
+  IterRangeCollector irc(range_, loop_iter_vars_);
+  irc.Visit(s);
+
+  std::unordered_map<const Variable*, Expr> vmap;
+  for (size_t k = 0; k < irc.loop_iter_vars_.size(); k++) {
+  }
+  Stmt stmt = Substitute(s, vmap);
+  return stmt;
+};
+
+// Collect tensor type and shape information
+class TypeShapeCollector final : public IRMutator {
+  public: 
+    TypeShapeCollector(Array<NodeRef>& api_args) {
+    for (size_t i = 0; i < api_args.size(); i++) { 
+      if (const Variable* v = api_args[i].as<Variable>()) {
+        top_arg_names.insert(v->name_hint);
+
+      } else if (auto buf = api_args[i].as<BufferNode>()) {
+        CHECK(buf->data.as<Variable>());
+        top_arg_names.insert(buf->name); 
+        shape_[buf->data.get()->name_hint] = buf->shape;
+        dtype_[buf->data.get()->name_hint] = buf->dtype;
+        HCL_DEBUG_LEVEL(2) << "  [ collect shape ] " << buf->name;
+      }
+    }
+  };
+
+  Stmt Mutate_(const Allocate *op, const Stmt& s) final {
+    auto v = op->buffer_var.get();
+    auto name = v->name_hint; 
+    // Save shape and dtype information
+    shape_[name] = op->extents;
+    dtype_[name] = op->type;
+    HCL_DEBUG_LEVEL(2) << "  [ collect shape ] " << name;
+    return IRMutator::Mutate_(op, s);
+  }
+
+  Stmt Mutate_(const KernelDef* op, const Stmt& s) {
+    for (size_t i = 0; i < op->args.size(); i++) {
+      string name = op->args[i].get()->name_hint;
+      auto shape = op->arg_shapes[i];
+      shape_[name] = shape;
+      CHECK(op->arg_types[i].as<StringImm>());
+      dtype_[name] = Str2Type(op->arg_types[i].as<StringImm>()->value);
+      HCL_DEBUG_LEVEL(2) << "  [ collect shape ] " << name;
+    }
+    return IRMutator::Mutate_(op, s);
+  }
+
+  Type Str2Type(string type_str) {
+    if (type_str.find("int") == 0) {
+      type_str.erase(0, 3);
+      int bits = std::atoi(type_str.c_str());
+      return Int(bits);
+    } else if (type_str.find("uint") == 0) {
+      type_str.erase(0, 4);
+      int bits = std::atoi(type_str.c_str());
+      return UInt(bits);
+    } else if (type_str.find("float") == 0) {
+      type_str.erase(0, 5);
+      int bits = std::atoi(type_str.c_str());
+      return Float(bits);
+    }
+    return Int(32);
+  }
+
+  unordered_set<string> top_arg_names;
+  unordered_map<string, Array<Expr> > shape_;
+  unordered_map<string, Type> dtype_;
+};
+
+
+void CollectTypeShape(Stmt body, unordered_map<string, Array<Expr> >& shape, 
+  unordered_map<string, Type>& dtype, Array<NodeRef>& api_args) {
+    HCL_DEBUG_LEVEL(2) << "---------- collect shape/dtype ---------";
+    TypeShapeCollector tsc(api_args);
+    tsc.Mutate(body);
+    dtype = tsc.dtype_;
+    shape = tsc.shape_;
 };
 
 // Check all the tensors in the Stmt. Get information
@@ -164,23 +270,71 @@ class TaskGraphBuilder : public IRMutator {
 class LayoutTransformer : public IRMutator {
  public:
   explicit LayoutTransformer(
-    unordered_map<string, TaskNode>& task_map)
-    : task_map_(task_map) { }
+    unordered_map<string, TaskNode>& task_map,
+    Array<NodeRef>& api_args)
+    : task_map_(task_map), api_args_(api_args) { }
+
   unordered_map<string, TaskNode>& task_map_;
+  Array<NodeRef>& api_args_;
+  unordered_map<string, Array<Expr> > shape_;
+  unordered_map<string, Type> dtype_;
 
   Stmt Mutate_(const AttrStmt* op, const Stmt& s) {
-
     // The tensor to be transformed
     if (op->attr_key == attr::tensor_layout_attrs) {
-
         VarExpr var(op->node.node_);
         auto name = var.get()->name_hint;
-        HCL_DEBUG_LEVEL(2) << "[ debug ] Transform layout of tensor " 
-          << name << "..." << op->body;
+        CHECK(shape_.count(name)) << name;
+        CHECK(dtype_.count(name)) << name;
+            
+        size_t pos = 0;
+        string delimiter = ":";
+        string token;
+        vector<int> target_shape;
 
+        CHECK(op->value.as<StringImm>());
+        string s(op->value.as<StringImm>()->value);
+
+        int target_total_width = 1;
+        while ((pos = s.find(delimiter)) != string::npos) {
+          token = s.substr(0, pos);
+          target_shape.push_back(std::stoi(token));
+          s.erase(0, pos + delimiter.length());
+          target_total_width *= std::stoi(token);
+        }
+        target_total_width *= std::stoi(s);
+
+        // Check tranform type (tranpose or packing)
+        int origin_total_width = 1;
+        for (auto& dim: shape_.at(name)) {
+          CHECK(dim.as<IntImm>());
+          origin_total_width *= dim.as<IntImm>()->value;
+        }
+
+        HCL_DEBUG_LEVEL(2) << origin_total_width << ", " << target_total_width;
+        // TODO(hecmay): handle reshape
+        if (origin_total_width == target_total_width) {
+          HCL_DEBUG_LEVEL(2) << "[ debug ] Transpose layout of tensor " 
+            << name << "(" << shape_[name] << ") to (" << 
+            op->value << ")";
+          // Just change the access order
+          // The (in-place) tranposition is done in another pass
+          return ReorderBufferAccess(op->body, name);
+
+        } else {
+          // Pack the last dimension by default
+          HCL_DEBUG_LEVEL(2) << "[ debug ] Pack layout of tensor " 
+            << name << "(" << shape_[name] << ") to (" << 
+            op->value << ")";
+        }
         return this->Mutate(op->body);
     }
     return IRMutator::Mutate_(op, s);
+  }
+
+  Stmt Transform(Stmt s) {
+    CollectTypeShape(s, shape_, dtype_, api_args_);
+    return this->Mutate(s);
   }
 
 };
@@ -193,8 +347,8 @@ Stmt TransformLayout(Stmt stmt, Array<NodeRef> api_args) {
 
   // Check the tensor in worklist (to be transposed or packed)
   // Here we create a new attribute statment key "tensor_view
-  LayoutTransformer ltm(tgb.task_map);
-  stmt = ltm.Mutate(stmt);
+  LayoutTransformer ltm(tgb.task_map, api_args);
+  stmt = ltm.Transform(stmt);
   return stmt;
 }
 
