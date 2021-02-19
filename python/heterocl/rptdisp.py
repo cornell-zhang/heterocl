8f1f19596eef38e0079ae0cc3eebf6dea4fd8bf7
YoungSeok Na <yn224@cornell.edu>
2021-02-18 21:28:40 -0500

    Docstring update

--- python/heterocl/rptdisp.py
+++ python/heterocl/rptdisp.py
@@ -3,71 +3,74 @@ from tabulate import tabulate
 
 class RptDisp(object):
   """
-  A class used for the report table that could be queried.
+  Queryable report displayer.
+
+  ...
 
   Attributes
   ----------
-  * _category (list)
-    List of default latency information category.
+  _category: list
+      List of default latency information category.
 
-  * _category_aux (list)
-    List of latency information category with range indicators.
+  _category_aux: list
+      List of latency information category with range indicators.
 
-  * _loop_name (list)
-    List of loop names without loop nest indicators.
+  _loop_name: list
+      List of loop names without loop nest indicators.
 
-  * _loop_name_aux (list)
-    List of loop names with loop nest indicators.
+  _loop_name_aux: list
+      List of loop names with loop nest indicators.
 
-  * _max_level (int)
-    Maximum level of loop nest in the report file.
+  _max_level: int
+      Maximum level of loop nest in the report file.
 
-  * _data (dict)
-    Dictionary containing latency data. 
+  _data: dict
+      Dictionary containing latency data. 
 
-  * unit (str)
-    Unit of each information.
+  unit: str
+      Unit of each information.
 
   Methods
   ----------
-  * __is_range(v, key)
-    Check whether the value is a range value or not.
+  __is_range(v, key)
+      Check whether the value is a range value or not.
 
-  * scan_range(obj)
-    Scan the entire report file to see which latency category contains
-    range values.
+  scan_range(obj)
+      Scan the entire report file to see which latency category contains
+      range values.
 
-  * __get_value(v, key, minmax)
-    Get the value associated with the input key.
+  __get_value(v, key, minmax)
+      Get the value associated with the input key.
 
-  * __info_extract(obj, key, minmax, col)
-    Extract out all the latency information from the report. 
+  __info_extract(obj, key, minmax, col)
+      Extract out all the latency information from the report. 
 
-  * init_data(obj)
-    Initialize the data given the report file.  
+  init_data(obj)
+      Initialize the data given the report file.  
 
-  * get_loops(obj)
-    Acquire loop names with and without loop nest indicators.
+  get_loops(obj)
+      Acquire loop names with and without loop nest indicators.
   
-  * __select_loops(loops)
-    Select only the loops specified by the user.
+  __select_loops(loops)
+      Select only the loops specified by the user.
   
-  * __select_levels(loops, level)
-    Select only the loops that are included in the level specified by
-    the user.
+  __select_levels(loops, level)
+      Select only the loops that are included in the level specified by
+      the user.
   
-  * __select_cols(cols)
-    Select only the columns specified by the user.
+  __select_cols(cols)
+      Select only the columns specified by the user.
   
-  * display(loops=None, level=None, cols=None)
-    Display the report table with appropriate query arguments.
+  display(loops=None, level=None, cols=None)
+      Display the report table with appropriate query arguments.
   """
 
   def __init__(self, unit):
     """
     Parameters
     ----------
-    * unit (str) - Unit for all numerical values in the table.
+    unit: str
+        Unit for all numerical values in the table.
     """
     self._category = ['Trip Count', 'Latency', 'Iteration Latency', 
                        'Pipeline II', 'Pipeline Depth']
@@ -79,42 +82,35 @@ class RptDisp(object):
     self.unit = unit
 
   def __is_range(self, v, key):    
-    """
-    Check whether the value is a range value or not.
+    """Check whether the value is a range value or not.
 
     Parameters
     ----------
-    * v   (dict) - Dictionary containing all latency information
-                   for a particular loop.
-    * key (str)  - Latency category.
+    v: dict
+        Dictionary containing all latency information for a particular loop.
+    key: str
+        Latency category.
 
     Returns
     ----------
-    True if the value is a range value. Otherwise, False. 
-
-    Return Type
-    ----------
-    bool 
+    bool
+      True if the value is a range value. Otherwise, False. 
     """
     return isinstance(v.get(key), dict)
 
   def scan_range(self, obj):
-    """
-    Scans the parsed xml file to check which categories have range 
+    """Scans the parsed xml file to check which categories have range 
     values and updates _category_aux accordingly. Also, it initializes
     _data to be used in displaying the report data.
 
     Parameters
     ----------
-    * obj (dict) - Dictionary representation of the report file.
+    obj: dict 
+        Dictionary representation of the report file.
 
     Returns
     ----------
     None
-
-    Return Type
-    ----------
-    None  
     """
     detect_minmax = []
     for item in self._category:
@@ -138,25 +134,23 @@ class RptDisp(object):
       self._data[c] = []
 
   def __get_value(self, v, key, minmax):
-    """
-    Gets the value associated with _key_. If the value is a range
+    """Gets the value associated with _key_. If the value is a range
     value, get the appropriate 'min' or 'max' value, determined by
     _minmax_.
 
     Parameters
     ----------
-    * v      (dict) - Dictionary containing all latency information
-                      for a particular loop.
-    * key    (str)  - Latency category.
-    * minmax (str)  - Range indicator (min or max).
+    v: dict
+        Dictionary containing all latency information for a particular loop.
+    key: str
+        Latency category.
+    minmax: str
+        Range indicator (min or max).
  
     Returns
     ----------
-    Latency value of the loop with category 'key'. 
-
-    Return Type
-    ----------
     str
+        Latency value of the loop with category 'key'. 
     """
     num = v.get( key )
     if isinstance( num, str ):
@@ -168,23 +162,22 @@ class RptDisp(object):
     return val
 
   def __info_extract(self, obj, key, minmax, col):
-    """
-    Extract out all the latency information from the report.
+    """Extract out all the latency information from the report.
 
     Parameters
     ----------
-    * obj    (dict) - Dictionary representation of the report file.
-    * key    (str)  - Latency category. 
-    * minmax (str)  - Range indicator (min or max). 
-    * col    (list) - Column name in the data.
+    obj: dict
+        Dictionary representation of the report file.
+    key: str
+        Latency category. 
+    minmax: str
+        Range indicator (min or max). 
+    col: list
+        Column name in the data.
     
     Returns
     ----------
     None
-
-    Return Type
-    ----------
-    None  
     """
     for k, v in obj.items():      
       val = self.__get_value(v, key, minmax)
@@ -196,20 +189,16 @@ class RptDisp(object):
         in_k, in_v = list(in_v.items())[-1]
 
   def init_data(self, obj):
-    """
-    Initialize the _data attribute.
+    """Initialize the _data attribute.
  
     Parameters
     ----------
-    * obj (dict) - Dictionary representation of the report file. 
+    obj: dict
+        Dictionary representation of the report file. 
     
     Returns
     ----------
     None
-
-    Return Type
-    ----------
-    None  
     """
     for col in self._category_aux:
       key_split = col.split(' ', 1)
@@ -224,20 +213,16 @@ class RptDisp(object):
       self.__info_extract(obj, info_tuple[0], info_tuple[1], col)
 
   def get_loops(self, obj):
-    """
-    Initializes the loop name lists.
+    """Initializes the loop name lists.
 
     Parameters
     ----------
-    * obj (dict) - Dictionary representation of the report file. 
+    obj: dict
+        Dictionary representation of the report file. 
 
     Returns
     ----------
     None
-
-    Return Type
-    ----------
-    None  
     """
     for k, v in obj.items():
       self._loop_name.append(k)
@@ -254,20 +239,17 @@ class RptDisp(object):
         self._max_level = n
 
   def __select_loops(self, loops):
-    """
-    Select only the loops specified by the user.
+    """Select only the loops specified by the user.
 
     Parameters
     ----------
-    * loops (list) - List of loop names.
+    loops: list
+        List of loop names.
     
     Returns
     ----------
-    List of specific pattern-matched loop names. 
-
-    Return Type
-    ----------
-    list  
+    list
+        List of specific pattern-matched loop names. 
     """
     selected = []
     for l in loops:
@@ -277,22 +259,19 @@ class RptDisp(object):
     return selected
   
   def __select_levels(self, loops, level):
-    """
-    Select only the loops that are within the range of level.
+    """Select only the loops that are within the range of level.
 
     Parameters
     ----------
-    * loops (list) - List of specified loop names.
-    * level (int)  - Number indicating the maximum loop nest level to
-                     print.
+    loops: list
+        List of specified loop names.
+    level: int
+        Number indicating the maximum loop nest level to print.
 
     Returns
     ----------
-    List of loops that are within the range of level.
-
-    Return Type
-    ----------
     list
+        List of loops that are within the range of level.
     """
     rows = []
     for k in loops:
@@ -302,20 +281,17 @@ class RptDisp(object):
     return rows 
 
   def __select_cols(self, cols):
-    """
-    TODO: Exact pattern matching
+    """Select specific latency category to be displayed.
 
     Parameters
     ----------
-    * cols (list) - List of specific column names.
+    cols: list
+        List of specific column names.
     
     Returns
     ----------
-    List of pattern-matched column names.
-
-    Return Type
-    ----------
     list
+        List of pattern-matched column names.
     """
     ncols = []
     for c in cols:
@@ -325,22 +301,20 @@ class RptDisp(object):
     return ncols
  
   def display(self, loops=None, level=None, cols=None):
-    """
-    Display the report file.
+    """Display the report file.
 
     Parameters
     ----------
-    * loops (list, optional) - List of loop names (e.g., ['A', 'Y'])
-    * level (int,  optional) - Maximum level of loop nest to print
-    * cols  (list, optional) - List of column names (e.g., ['Trip Count'])
+    loops: list, optional
+        List of loop names (e.g., ['A', 'Y'])
+    level: int, optional
+        Maximum level of loop nest to print.
+    cols: list, optional
+        List of column names. (e.g., ['Trip Count'])
  
     Returns
     ----------
     None
-
-    Return Type
-    ----------
-    None  
     """
     if loops is None:
       loops = self._loop_name_aux
@@ -351,9 +325,7 @@ class RptDisp(object):
 
     selected = self.__select_loops(loops)
     rows = self.__select_levels(selected, level)
-    # TODO: one could either provide the default one of the five or the exact 
-    #       _select_cols buggy currently
-    #ncols = self.__select_cols(cols)
+    ncols = self.__select_cols(cols)
     alignment = ('left',)
     for i in range(len(cols)):
       alignment = alignment + ('right',)
