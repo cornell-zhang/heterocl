ca043455f482e78ded58e2ebea807f6ddbd2e11d
seanlatias <seanlatias@gmail.com>
2021-03-18 18:56:47 -0400

    fix lint errors

--- include/hcl/dialect/HCLIR/HCLIRDialect.h
+++ include/hcl/dialect/HCLIR/HCLIRDialect.h
@@ -4,11 +4,11 @@
 //  Dialect for HeteroCL IR
 //===---------------------------------------------===//
 
-#ifndef HCL_DIALECT_HIR_HCLIRDIALECT_H_
-#define HCL_DIALECT_HIR_HCLIRDIALECT_H_
+#ifndef HCL_DIALECT_HCLIR_HCLIRDIALECT_H_
+#define HCL_DIALECT_HCLIR_HCLIRDIALECT_H_
 
 #include <mlir/IR/Dialect.h>
 
 #include "hcl/dialect/HCLIR/HCLIROpsDialect.h.inc"
 
-#endif  // HCL_DIALECT_HIR_HCLIRDIALECT_H_
+#endif  // HCL_DIALECT_HCLIR_HCLIRDIALECT_H_
