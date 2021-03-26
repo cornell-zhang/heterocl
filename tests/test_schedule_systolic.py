02da5886123f3f82cb332bade8d68d4a077756b3
Your Name <you@example.com>
2021-03-26 01:32:02 +0000

    remove typo in test cases

--- tests/test_schedule_systolic.py
+++ tests/test_schedule_systolic.py
@@ -348,7 +348,7 @@ def test_unroll_outer_loops():
     code = str(hcl.lower(s))
 
 if __name__ == '__main__':
-    test_stencil_stream(); de
+    test_stencil_stream()
     test_free_running_kernel()
     test_compose_systolic_arrays()
     test_autosa_schedule()
