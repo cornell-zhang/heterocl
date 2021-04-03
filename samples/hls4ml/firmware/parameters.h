ae07d490da53e87a37b7ae4ee1ae652e0c8d8252
Hecmay <sx233@cornell.edu>
2021-04-03 14:04:52 -0400

    add parameter.h to toyNN ip example

---
+++ samples/hls4ml/firmware/parameters.h
@@ -0,0 +1,116 @@
+#ifndef PARAMETERS_H_
+#define PARAMETERS_H_
+
+#include "ap_int.h"
+#include "ap_fixed.h"
+
+#include "nnet_utils/nnet_helpers.h"
+//hls-fpga-machine-learning insert includes
+#include "nnet_utils/nnet_activation.h"
+#include "nnet_utils/nnet_dense.h"
+#include "nnet_utils/nnet_dense_compressed.h"
+#include "nnet_utils/nnet_dense_large.h"
+ 
+//hls-fpga-machine-learning insert weights
+#include "weights/w2.h"
+#include "weights/b2.h"
+#include "weights/w5.h"
+#include "weights/b5.h"
+#include "weights/w8.h"
+#include "weights/b8.h"
+#include "weights/w11.h"
+#include "weights/b11.h"
+
+//hls-fpga-machine-learning insert layer-config
+struct config2 : nnet::dense_config {
+    static const unsigned n_in = N_INPUT_1_1;
+    static const unsigned n_out = N_LAYER_2;
+    static const unsigned io_type = nnet::io_parallel;
+    static const unsigned reuse_factor = 1;
+    static const unsigned n_zeros = 0;
+    static const unsigned n_nonzeros = 1024;
+    static const bool store_weights_in_bram = false;
+    typedef ap_fixed<16,6> accum_t;
+    typedef model_default_t bias_t;
+    typedef model_default_t weight_t;
+    typedef ap_uint<1> index_t;
+};
+
+struct relu_config4 : nnet::activ_config {
+    static const unsigned n_in = N_LAYER_2;
+    static const unsigned table_size = 1024;
+    static const unsigned io_type = nnet::io_parallel;
+    static const unsigned reuse_factor = 1;
+    typedef ap_fixed<18,8> table_t;
+};
+
+struct config5 : nnet::dense_config {
+    static const unsigned n_in = N_LAYER_2;
+    static const unsigned n_out = N_LAYER_5;
+    static const unsigned io_type = nnet::io_parallel;
+    static const unsigned reuse_factor = 1;
+    static const unsigned n_zeros = 0;
+    static const unsigned n_nonzeros = 2048;
+    static const bool store_weights_in_bram = false;
+    typedef ap_fixed<16,6> accum_t;
+    typedef model_default_t bias_t;
+    typedef model_default_t weight_t;
+    typedef ap_uint<1> index_t;
+};
+
+struct relu_config7 : nnet::activ_config {
+    static const unsigned n_in = N_LAYER_5;
+    static const unsigned table_size = 1024;
+    static const unsigned io_type = nnet::io_parallel;
+    static const unsigned reuse_factor = 1;
+    typedef ap_fixed<18,8> table_t;
+};
+
+struct config8 : nnet::dense_config {
+    static const unsigned n_in = N_LAYER_5;
+    static const unsigned n_out = N_LAYER_8;
+    static const unsigned io_type = nnet::io_parallel;
+    static const unsigned reuse_factor = 1;
+    static const unsigned n_zeros = 0;
+    static const unsigned n_nonzeros = 1024;
+    static const bool store_weights_in_bram = false;
+    typedef ap_fixed<16,6> accum_t;
+    typedef model_default_t bias_t;
+    typedef model_default_t weight_t;
+    typedef ap_uint<1> index_t;
+};
+
+struct relu_config10 : nnet::activ_config {
+    static const unsigned n_in = N_LAYER_8;
+    static const unsigned table_size = 1024;
+    static const unsigned io_type = nnet::io_parallel;
+    static const unsigned reuse_factor = 1;
+    typedef ap_fixed<18,8> table_t;
+};
+
+struct config11 : nnet::dense_config {
+    static const unsigned n_in = N_LAYER_8;
+    static const unsigned n_out = N_LAYER_11;
+    static const unsigned io_type = nnet::io_parallel;
+    static const unsigned reuse_factor = 1;
+    static const unsigned n_zeros = 0;
+    static const unsigned n_nonzeros = 160;
+    static const bool store_weights_in_bram = false;
+    typedef ap_fixed<16,6> accum_t;
+    typedef model_default_t bias_t;
+    typedef model_default_t weight_t;
+    typedef ap_uint<1> index_t;
+};
+
+struct softmax_config13 : nnet::activ_config {
+    static const unsigned n_in = N_LAYER_11;
+    static const unsigned table_size = 1024;
+    static const unsigned io_type = nnet::io_parallel;
+    static const unsigned reuse_factor = 1;
+    static const nnet::softmax_implementation implementation = nnet::softmax_implementation::latency;
+    typedef ap_fixed<18,8> exp_table_t;
+    typedef ap_fixed<18,8> inv_table_t;
+};
+
+
+#endif
