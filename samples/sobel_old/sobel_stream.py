d2a274294ec166f5e9f892e5ad9db582fb7afed2
YoungSeok Na <yn224@cornell.edu>
2021-07-07 11:17:09 -0400

    [Tutorial] Sobel FIFO stream update

--- samples/sobel_old/sobel_stream.py
+++ samples/sobel_old/sobel_stream.py
@@ -14,28 +14,43 @@ Gx = hcl.placeholder((3,3), "Gx",dtype=hcl.Float())
 Gy = hcl.placeholder((3,3), "Gy",dtype=hcl.Float())
 
 def sobel(A, Gx, Gy):
+    r = hcl.reduce_axis(0,3)
+    c = hcl.reduce_axis(0,3)
 
-   r = hcl.reduce_axis(0,3)
-   c = hcl.reduce_axis(0,3)
-   B = hcl.compute((height-2,width-2), 
-           lambda x,y: hcl.sum(A[x+r,y+c]*Gx[r,c], axis=[r,c], name="sum1"),
-           name="B", dtype=hcl.Float())
-   t = hcl.reduce_axis(0,3)
-   g = hcl.reduce_axis(0,3)
+    B1 = hcl.compute((height-2,width-2), 
+            lambda x,y: hcl.sum(A[x+r,y+c]*Gx[r,c], axis=[r,c], name="sum1"),
+            name="B1", dtype=hcl.Float())
+    
+    t = hcl.reduce_axis(0,3)
+    g = hcl.reduce_axis(0,3)
+    
+    B2 = hcl.compute((height-2,width-2), 
+            lambda x,y: hcl.sum(A[x+t,y+g]*Gy[t,g], axis=[t,g], name="sum2"),
+            name="B2", dtype=hcl.Float())
+    
+    def avg(in1, in2):
+        ll = hcl.scalar(in1, "in1")
+        lr = hcl.scalar(in2, "in2")
+        return hcl.sqrt(ll.v * ll.v + lr.v * lr.v)/4328*255
+    
+    return hcl.compute((height-2,width-2), 
+               lambda x, y : avg(B1[x,y], B2[x,y]),
+               name="output", dtype=hcl.Float())
 
-   C = hcl.compute((height-2,width-2), 
-           lambda x,y: hcl.sum(A[x+t,y+g]*Gy[t,g], axis=[t,g], name="sum2"),
-           name="C", dtype=hcl.Float())
-   return hcl.compute((height-2,width-2), 
-              lambda x, y :hcl.sqrt(B[x,y]*B[x,y] + C[x,y]*C[x,y])/4328*255,
-              name="output", dtype=hcl.Float())
+target = hcl.Platform.aws_f1
+target.config(compiler="vitis", backend="vhls")
 
-target = hcl.platform.aws_f1
-target.config(compile="vitis", backend="vhls")
+s = hcl.create_schedule([A, Gx, Gy], sobel)
 
-s = hcl.create_schedule([A,Gx,Gy], sobel)
-s.to([Gx, Gy, A], target.xcel)
+# Move inputs to FPGA and output back to CPU
+s.to(A, target.xcel, burst_len=128).to([sobel.B1, sobel.B2])
+s.to([Gx, Gy], target.xcel)
 s.to(sobel.output, target.host)
+
+# Create FIFO channels between sub-kernels
+s.to(sobel.B1, sobel.output)
+s.to(sobel.B2, sobel.output)
+
 f = hcl.build(s, target)
 
 npA = np.array(img)
