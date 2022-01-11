fc5ea8ccd9a315aa247a5cee579c426c4009b8b1
chhzh123 <hc676@cornell.edu>
2022-01-10 19:20:26 -0500

    Add Loop/StageHandle & AffineYieldOp

--- python/heterocl/build_mlir.py
+++ python/heterocl/build_mlir.py
@@ -6,6 +6,7 @@ import inspect
 from .schedule import Stage
 from .base import get_context, get_loc, get_module, get_function, get_func_body
 from mlir.dialects import builtin, std, memref
+import hcl_mlir.affine as affine
 import io
 
 def compute_body(name,
@@ -20,7 +21,6 @@ def compute_body(name,
     return_tensor = True if tensor is None else False
 
     ret = fcompute(*var_list)
-    print(ret, type(ret))
     
     with Context() as ctx, Location.unknown() as loc:
         body = get_func_body()
@@ -56,24 +56,29 @@ def compute_mlir(shape, fcompute, name=None, dtype=None, attrs=OrderedDict()):
     assert argspec.defaults is None, "Default arguments not supported in fcompute"
     assert len(argspec.kwonlyargs) == 0, "Keyword arguments are not supported in fcompute"
 
-    with get_context(), get_loc():
+    with get_context() as ctx, get_loc() as loc:
         with Stage(name, dtype, shape) as stage:
-            stage_handle = hcl_mlir.CreateStageHandleOp(F32Type.get(), StringAttr.get(name), ip=InsertionPoint(get_func_body()))
+            loop_handle_type = hcl_mlir.StageHandleType.get(ctx)
+            stage_handle = hcl_mlir.CreateStageHandleOp(loop_handle_type, StringAttr.get(name), ip=InsertionPoint(get_func_body()))
             stage.stage_handle = stage_handle
             loop_handles = []
             for loop_name in arg_names:
-                loop_handles.append(hcl_mlir.CreateLoopHandleOp(F32Type.get(), StringAttr.get(loop_name), ip=InsertionPoint(get_func_body())))
+                loop_handles.append(hcl_mlir.CreateLoopHandleOp(hcl_mlir.LoopHandleType.get(ctx), StringAttr.get(loop_name), ip=InsertionPoint(get_func_body())))
 
             loops = []
             body = get_func_body()
             for i, (ub, loop_name) in enumerate(zip(shape, arg_names)):
                 loop = hcl_mlir.make_constant_for(0, ub, step=1, name=loop_name, stage=(name if i == 0 else ""), ip=InsertionPoint(body))
+                if i != 0: # manually add terminator!
+                    affine.AffineYieldOp([], ip=InsertionPoint(body))
                 loops.append(loop)
                 body = loop.body
 
             iter_var = [hcl_mlir.IterVar(loop.induction_variable, InsertionPoint(body)) for loop in loops]
             ret = fcompute(*iter_var)
             ret_val = memref.StoreOp(ret.op.result, ret_tensor.op.result, [loop.induction_variable for loop in loops], ip=InsertionPoint(body))
+            # remember to add affine.yield after each for loop
+            affine.AffineYieldOp([], ip=InsertionPoint(body))
             # hard coded
             stage.mlir_axis = loop_handles
             return ret_val
@@ -85,8 +90,9 @@ def build_hlsc(schedule, target=None, name="default_function", stmt=None):
     get_module().dump()
     # lowering
     func = get_function()
+    # https://llvm.discourse.group/t/open-discussion-on-mlir-bindings/3159/19
     hcl_mlir.loop_transformation(func.operation)
-    print("Done optimization")
+    get_module().dump()
     # generate code
     buf = io.StringIO()
     hcl_mlir.emit_hlscpp(get_module(), buf)
