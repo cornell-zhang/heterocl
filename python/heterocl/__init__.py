1aa70c1eb3680092339d614d538e8e8b175d0336
chhzh123 <hc676@cornell.edu>
2022-01-15 17:12:47 -0500

    Add IR selection

--- python/heterocl/__init__.py
+++ python/heterocl/__init__.py
@@ -1,17 +1,29 @@
-import sys
-# from .api import *
-from .mlir.schedule import *
-from .mlir.build_module import *
-from .mlir.operation import *
-from .mlir.runtime import *
-# from .dsl import *
-from .types import *
-# from .devices import *
-# from .platforms import *
-# from .nparray import *
-# from .debug import hcl_excepthook
-# from .tvm.intrin import *
-# from .tvm.stmt import Partition
-# from .tvm.expr import IO
+import sys, os
 
-# sys.excepthook = hcl_excepthook
+if "HCLIR" in os.environ:
+    ir_name = os.getenv('HCLIR')
+else:
+    ir_name = "mlir"
+
+IR = ir_name
+print("Using {} as IR".format(IR))
+
+if ir_name == "mlir":
+    from .mlir.schedule import *
+    from .mlir.build_module import *
+    from .mlir.operation import *
+    from .mlir.runtime import *
+    from .types import *
+else:
+    from .api import *
+    from .compute_api import *
+    from .types import *
+    from .dsl import *
+    from .devices import *
+    from .platforms import *
+    from .nparray import *
+    from .debug import hcl_excepthook
+    from .tvm.intrin import *
+    from .tvm.stmt import Partition
+    from .tvm.expr import IO
+    sys.excepthook = hcl_excepthook
