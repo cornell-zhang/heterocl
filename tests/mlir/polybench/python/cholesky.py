b1756ecad0cfa3c0dc079e3ec6d6f0d0b75092c8
Niansong Zhang <nz264@cornell.edu>
2022-01-30 00:24:02 -0500

    [Test] Cholesky passed! :tada::tada::tada:

--- tests/mlir/polybench/python/cholesky.py
+++ tests/mlir/polybench/python/cholesky.py
@@ -8,7 +8,6 @@ def top_cholesky(N, dtype=hcl.Int(), target=None):
     A = hcl.placeholder((N, N), "A")
 
     def kernel_cholesky(A):
-
         with hcl.Stage("loop_1"):
             with hcl.for_(0, N, name="i") as i:
                 # Case: j < i
@@ -17,17 +16,14 @@ def top_cholesky(N, dtype=hcl.Int(), target=None):
                         A[i][j] = A[i][j] -  A[i][k] * A[j][k]
                     A[i][j] = A[i][j] / A[j][j]
                 # Case: i == j
-                with hcl.for_(0, i, name="k") as k:
-                    A[i][i] = A[i][i] -  A[i][k] * A[i][k]
+                with hcl.Stage("k"):
+                    with hcl.for_(0, i, name="k") as k:
+                        A[i][i] = A[i][i] -  A[i][k] * A[i][k]
                 A[i][i] = hcl.sqrt(A[i][i] * 1.0)
+
         
     s = hcl.create_schedule([A], kernel_cholesky)
-
-    #### Apply customizations ####
-    
     loop_1 = kernel_cholesky.loop_1
-
-    #### Apply customizations ####
 
     return hcl.build(s, target=target)
 
@@ -42,15 +38,19 @@ def cholesky_golden(N, A):
 
         for k in range(i):
             A[i][i] -= A[i][k] * A[i][k]
-
         A[i][i] = mt.sqrt(A[i][i])
 
-def main(N=32, dtype=hcl.Float(32), target=None):
+def main(N=2, dtype=hcl.Float(32), target=None):
+    # Cholesky input matrix needs to be 
+    # symmetric and positive-definite.
     A = np.random.randint(10, size=(N, N)).astype(np.float32)
+    A_np = np.matmul(A, A.T)
+    # A_np is our random Hermitian matrix
+    A = np.copy(A_np)
+    A_golden = np.copy(A_np)
+    cholesky_golden(N, A_golden)
     f = top_cholesky(N, dtype, target)
     f(A)
-    A_golden = np.zeros(A.shape, dtype=np.float32)
-    cholesky_golden(N, A)
     if np.allclose(A, A_golden):
         print("pass")
     else:
