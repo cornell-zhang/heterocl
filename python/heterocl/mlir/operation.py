ed6e19bf040c25dde64c1fb311ddcc676b81502c
Niansong Zhang <nz264@cornell.edu>
2022-03-19 11:18:10 -0400

    [API][Print] Adding initial support for hcl.print

--- python/heterocl/mlir/operation.py
+++ python/heterocl/mlir/operation.py
@@ -9,19 +9,18 @@ from .context import UniqueName
 from .schedule import Schedule
 from .tensor import Array, Tensor
 from .utils import get_dtype_str, hcl_dtype_to_mlir
+from hcl_mlir.dialects import hcl as hcl_d
 from .dsl import for_
 
 
 def init(init_dtype=types.Int(32), raise_assert_exception=True):
-    """Initialize a HeteroCL environment with configurations.
-    """
+    """Initialize a HeteroCL environment with configurations."""
     config.init_dtype = init_dtype
     config.raise_assert_exception = raise_assert_exception
 
 
 def placeholder(shape, name=None, dtype=None):
-    """Construct a HeteroCL placeholder for inputs/outputs.
-    """
+    """Construct a HeteroCL placeholder for inputs/outputs."""
     if name is None:
         name = UniqueName.get("tensor")
     if not dtype == None and not isinstance(dtype, (Type, str)):
@@ -39,7 +38,7 @@ def asarray(np_array, dtype=None):
 
 
 def scalar(init, name=None, dtype=None):
-    """Syntactic sugar: single-value tensor 
+    """Syntactic sugar: single-value tensor
     - init: int, float, or expr
     """
     hcl_mlir.enable_build_inplace()
@@ -56,8 +55,7 @@ def scalar(init, name=None, dtype=None):
 
 
 def reduce_axis(lower, upper, name=None):
-    """Create a reduction axis for reduction operations.
-    """
+    """Create a reduction axis for reduction operations."""
     return hcl_mlir.ReduceVar(None, bound=(lower, upper), name=name)
 
 
@@ -82,8 +80,7 @@ def min(data, axis=None, dtype=None, name=""):
 
 
 def pack(tensor, axis=0, factor=None, name=None, dtype=None):
-    """Pack a tensor with smaller bitwidth to a tensor with larger bitwidth.
-    """
+    """Pack a tensor with smaller bitwidth to a tensor with larger bitwidth."""
     if factor is None or not isinstance(factor, int):
         raise RuntimeError("Should specify factor")
     if not isinstance(tensor.dtype, (Int, UInt)):
@@ -95,23 +92,25 @@ def pack(tensor, axis=0, factor=None, name=None, dtype=None):
         new_type = Int(bitwidth * factor)
     else:
         new_type = UInt(bitwidth * factor)
-    new_shape = [size // factor if i == axis else size for i,
-                 size in enumerate(tensor.shape)]
+    new_shape = [
+        size // factor if i == axis else size for i, size in enumerate(tensor.shape)
+    ]
 
     def assign_val(*indices):
-        result = scalar(0, name="packed_"+name, dtype=new_type)
+        result = scalar(0, name="packed_" + name, dtype=new_type)
         with for_(0, factor) as i:
-            new_indices = [index if j == axis else (
-                index*factor+i) for j, index in enumerate(indices)]
-            result[0][bitwidth*i:bitwidth*(i+1)] = tensor[tuple(new_indices)]
+            new_indices = [
+                index if j == axis else (index * factor + i)
+                for j, index in enumerate(indices)
+            ]
+            result[0][bitwidth * i : bitwidth * (i + 1)] = tensor[tuple(new_indices)]
         return result[0]
 
     return compute(tuple(new_shape), assign_val, name, new_type)
 
 
 def unpack(tensor, axis=0, factor=None, name=None, dtype=None):
-    """Unpack a tensor with larger bitwidth to a tensor with smaller bitwidth.
-    """
+    """Unpack a tensor with larger bitwidth to a tensor with smaller bitwidth."""
     if factor is None or not isinstance(factor, int):
         raise RuntimeError("Should specify factor")
     if not isinstance(tensor.dtype, (Int, UInt)):
@@ -123,16 +122,18 @@ def unpack(tensor, axis=0, factor=None, name=None, dtype=None):
         new_type = Int(bitwidth // factor)
     else:
         new_type = UInt(bitwidth // factor)
-    new_shape = [size // factor if i == axis else size for i,
-                 size in enumerate(tensor.shape)]
+    new_shape = [
+        size // factor if i == axis else size for i, size in enumerate(tensor.shape)
+    ]
 
     def assign_val(*indices):
-        result = scalar(0, name="unpacked_"+name, dtype=new_type)
-        new_indices = [index if j == axis else (
-            index//factor) for j, index in enumerate(indices)]
-        lower = (indices[axis] % factor) * (bitwidth//factor)
-        upper = lower + bitwidth//factor
-        result[0][0:bitwidth//factor] = tensor[tuple(new_indices)][lower:upper]
+        result = scalar(0, name="unpacked_" + name, dtype=new_type)
+        new_indices = [
+            index if j == axis else (index // factor) for j, index in enumerate(indices)
+        ]
+        lower = (indices[axis] % factor) * (bitwidth // factor)
+        upper = lower + bitwidth // factor
+        result[0][0 : bitwidth // factor] = tensor[tuple(new_indices)][lower:upper]
         return result[0]
 
     return compute(tuple(new_shape), assign_val, name, new_type)
@@ -151,8 +152,7 @@ def compute(shape, fcompute, name=None, dtype=None, attrs=OrderedDict()):
     if not dtype == None and not isinstance(dtype, (Type, str)):
         raise RuntimeError("Type error")
     dtype = config.init_dtype if dtype == None else dtype
-    ret_tensor = Tensor(shape, dtype, name=name,
-                        fcompute=fcompute, impl="compute")
+    ret_tensor = Tensor(shape, dtype, name=name, fcompute=fcompute, impl="compute")
     for tensor in ret_tensor.op.inputs:
         tensor.add_use(ret_tensor)
     return ret_tensor
@@ -165,12 +165,22 @@ def update(tensor: Tensor, fcompute, name=None):
     """
     # Check tensor type
     if not isinstance(tensor, Tensor):
-        raise RuntimeError("Unexpected argument type of the " +
-                           "first argument: {}, update API expects tensor as input.".format(type(tensor)))
+        raise RuntimeError(
+            "Unexpected argument type of the "
+            + "first argument: {}, update API expects tensor as input.".format(
+                type(tensor)
+            )
+        )
     if name is None:
         name = tensor.name + "_updated"
-    new_tensor = Tensor(tensor.shape, tensor.dtype, fcompute=fcompute,
-                        name=name, impl="compute", output=tensor)
+    new_tensor = Tensor(
+        tensor.shape,
+        tensor.dtype,
+        fcompute=fcompute,
+        name=name,
+        impl="compute",
+        output=tensor,
+    )
     tensor.add_use(new_tensor)
     Schedule._CurrentSchedule.DataflowGraph.add_edges(tensor, new_tensor)
 
@@ -184,8 +194,7 @@ def mutate(domain, fcompute, name=None):
         raise RuntimeError("The domain of mutate API must be a tuple")
     if name is None:
         name = UniqueName.get("tensor")
-    ret_tensor = Tensor(domain, None, name=name,
-                        fcompute=fcompute, impl="compute")
+    ret_tensor = Tensor(domain, None, name=name, fcompute=fcompute, impl="compute")
     return ret_tensor
 
 
@@ -193,7 +202,7 @@ def bitcast(tensor, dst_dtype, name=None):
     """Bitcast a HeteroCL tensor or expression to the destination data type of the same bitwidth.
     This API **bitcast** the input tensor from its own data type (source dtype)
     to the destination data type (dst_dtype). The destination data type must have
-    the same bitwidth with the source datatype. 
+    the same bitwidth with the source datatype.
     """
     if not isinstance(tensor, Tensor) and not isinstance(tensor, hcl_mlir.ExprOp):
         raise RuntimeError("bitcast input must be HeteroCL Tensor or ExprOp.")
@@ -209,16 +218,19 @@ def bitcast(tensor, dst_dtype, name=None):
         src_bitwidth = hcl_mlir.get_bitwidth(tensor.dtype)
     dst_bitwidth = dst_dtype.bits
     if src_bitwidth != dst_bitwidth:
-        raise RuntimeError("Destination datatype bitwidth does not match source bitwidth:" +
-                           f"source bitwidth: {src_bitwidth} , destination bitwidth {dst_bitwidth}.")
+        raise RuntimeError(
+            "Destination datatype bitwidth does not match source bitwidth:"
+            + f"source bitwidth: {src_bitwidth} , destination bitwidth {dst_bitwidth}."
+        )
 
     # set up name, shape, and fcompute
     dst_dtype_str = get_dtype_str(dst_dtype)
     if isinstance(tensor, Tensor):
-        name = tensor.name + '_' + dst_dtype_str if name is None else name
+        name = tensor.name + "_" + dst_dtype_str if name is None else name
         shape = tensor.shape
         fcompute = lambda *args: hcl_mlir.BitCastOp(
-            hcl_dtype_to_mlir(dst_dtype), tensor[args])
+            hcl_dtype_to_mlir(dst_dtype), tensor[args]
+        )
         return compute(shape, fcompute, name=name, dtype=dst_dtype)
     else:
         bitcast = hcl_mlir.BitCastOp(hcl_dtype_to_mlir(dst_dtype), tensor)
@@ -226,3 +238,8 @@ def bitcast(tensor, dst_dtype, name=None):
         builder.visit(bitcast)
         # return an expression
         return bitcast
+
+
+def print(vals, format=""):
+    printOp = hcl_mlir.PrintOp(vals, get_dtype_str(vals.dtype))
+    return printOp
