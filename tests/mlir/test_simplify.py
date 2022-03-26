f6054c559e3d455c402415b4e869b2b85dd1ccbc
chhzh123 <hc676@cornell.edu>
2022-03-26 17:33:04 -0400

    [Test] Fix test_simplify.py

--- tests/mlir/test_simplify.py
+++ tests/mlir/test_simplify.py
@@ -1,4 +1,5 @@
 import heterocl as hcl
+import pytest
 
 def test_remove_single_loop():
     hcl.init()
@@ -9,10 +10,10 @@ def test_remove_single_loop():
     assert "0 to 1" not in str(ir)
 
 def test_simplify_slice():
-    hcl.init()
-    A = hcl.placeholder((10,), "A")
-    def kernel(A):
-        A[5][2:2] = 4
-    s = hcl.create_schedule(A, kernel)
-    ir = hcl.lower(s)
-    assert "set_slice" not in str(ir)
+    with pytest.raises(RuntimeError):
+        hcl.init()
+        A = hcl.placeholder((10,), "A")
+        def kernel(A):
+            A[5][2:2] = 4
+        s = hcl.create_schedule(A, kernel)
+        ir = hcl.lower(s)
