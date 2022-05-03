46e4935a2ef01060a7f571d39f18e83d470beef7
Niansong Zhang <nz264@cornell.edu>
2022-05-03 16:30:28 -0400

    [Fix] Add assertions in the test case for #458

--- tests/issues/test_issue_458.py
+++ tests/issues/test_issue_458.py
@@ -1,15 +1,14 @@
 import heterocl as hcl
 
 def test():
-    # generates: assert ((uint32)0 < uint32(A[0]));
-    # the second uint32 is oddly specified ...
     def func(A):
         hcl.assert_(A[0] > 0, "")
 
     A = hcl.placeholder((2,), "A", dtype=hcl.UInt(16))
     s = hcl.create_schedule([A], func)
-    m = hcl.build(s, "shls")
-    print(m)
+    code = hcl.build(s, "shls")
+    assert "SC_ASSERT" in code
+    assert "uint32" not in code
 
 if __name__ == "__main__":
     test()
