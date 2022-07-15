e62a93ee7333f03a852a8eea3af01cab3d8b255b
Niansong Zhang <nz264@cornell.edu>
2022-07-15 16:10:30 -0400

    [Test] Add test_exceptions tests

--- tests/mlir/test_compute_basic.py
+++ tests/mlir/test_compute_basic.py
@@ -377,19 +377,3 @@ def test_const_tensor_fixed():
         test_kernel(hcl.UFixed(bit, 4), (8, 8))
         test_kernel(hcl.Fixed(bit, 4), (20, 20, 3))
         test_kernel(hcl.UFixed(bit, 4), (20, 20, 3))
-
-
-def test_deprecate_stage():
-    with warnings.catch_warnings(record=True) as w:
-        hcl.init(hcl.Int(32))
-        A = hcl.placeholder((10,))
-
-        def kernel(A):
-            with hcl.Stage("B"):
-                with hcl.for_(0, 10) as i:
-                    A[i] = A[i] + 1
-            return
-        s = hcl.create_schedule([A], kernel)
-        assert len(w) == 1
-        assert issubclass(w[-1].category, DeprecationWarning)
-        assert "deprecated" in str(w[-1].message)
