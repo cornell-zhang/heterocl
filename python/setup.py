e8d4a8a64b0b12c4817b5a922f438b7d825c7d44
chhzh123 <hc676@cornell.edu>
2022-08-03 00:51:38 -0400

    Update CI for LLVM 15

--- python/setup.py
+++ python/setup.py
@@ -4,14 +4,12 @@ setup(
   name = "heterocl",
   version = "1.0.0",
   install_requires=[
-      'numpy==1.18.5',
-      'decorator==4.4.2',
-      'networkx==2.5.1',
-      'matplotlib',
-      'backports.functools_lru_cache',
-      'ordered_set',
+      'numpy',
       'xmltodict',
       'tabulate',
-      'sodac',
-    #   'pandas', # timed out on CI
+      'pytest',
+      'networkx',
+      'matplotlib',
+      'pandas',
+      'imageio'
       ])
