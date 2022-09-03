0731cdd3bcdcc795fcec0e45dcb91528de978acb
Niansong Zhang <nz264@cornell.edu>
2022-09-03 11:50:06 -0400

    [Op][Print] Support empty tuple

--- python/heterocl/print.py
+++ python/heterocl/print.py
@@ -14,22 +14,25 @@ def print(vals, format_str=""):
     elif isinstance(vals, int) or isinstance(vals, float):
         dtype = Int(64) if isinstance(vals, int) else Float(64)
         value = hcl_mlir.ConstantOp(get_dtype_str(dtype), vals)
-        printOp = hcl_mlir.PrintOp([value])
         if format_str == "":
             format_str = "%d \n" if isinstance(vals, int) else "%.3f \n"
+        printOp = hcl_mlir.PrintOp([value], format_str)
     elif isinstance(vals, hcl_mlir.build_ir.ExprOp):
         # When vals is an expression
-        printOp = hcl_mlir.PrintOp([vals])
         if format_str == "":
             if "int" in get_dtype_str(vals.dtype):
                 format_str = "%d \n"
             else:
                 format_str = "%.3f \n"
+        printOp = hcl_mlir.PrintOp([vals], format_str)
     elif isinstance(vals, (tuple, list)):
         # When vals is an tuple
         if isinstance(vals, tuple):
             vals = list(vals)
-        printOp = hcl_mlir.PrintOp(vals)
+        # when vals is empty
+        if len(vals) == 0:
+            value = hcl_mlir.ConstantOp('int32', 0)
+            vals = [value]
         if format_str == "":
             for v in vals:
                 if "int" in get_dtype_str(v.dtype):
@@ -37,9 +40,7 @@ def print(vals, format_str=""):
                 else:
                     format_str += "%.3f "
             format_str += "\n"
+        printOp = hcl_mlir.PrintOp(vals, format_str)
     else:
         raise HCLValueError(f"Unsupported type for print: {vals}")
-    # Attach format string as an attribute
-    if format_str != "":
-        printOp.built_op.attributes["format"] = StringAttr.get(format_str)
     return printOp
