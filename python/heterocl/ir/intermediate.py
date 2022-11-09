40d9022aa75727f61079ddcb2b01c6a43eedd76a
Niansong Zhang <nz264@cornell.edu>
2022-11-09 17:32:30 -0500

    [Intermediate] Intermediate layer initial commit

--- python/heterocl/ir/intermediate.py
+++ python/heterocl/ir/intermediate.py
@@ -6,8 +6,26 @@
 import os
 import sys
 import inspect
-from ..utils import get_src_loc
+from ..utils import get_src_loc, hcl_dtype_to_mlir
+from hcl_mlir.exceptions import *
+from ..context import *
+from ..types import *
 
+def print_indent(string, level):
+    for _ in range(level):
+        string += "  "
+    return string
+
+def immediate_to_constant(value, loc):
+    if not isinstance(value, (int, float)):
+        return value # pass through
+    if isinstance(value, int):
+        if value < 0xFFFFFFFF:
+            return ConstantOp(value, Int(32), loc)
+        else:
+            return ConstantOp(value, Int(64), loc)
+    else:
+        return ConstantOp(value, Float(64), loc)
 
 class Location(object):
     """ Filename and linenumber
@@ -18,6 +36,34 @@ class Location(object):
 
     def __str__(self):
         return f"{self.filename}:{self.lineno}"
+
+class Scope(object):
+    """ Scope class to manage operation insertion scopes."""
+    def __init__(self):
+        self.stack = list()
+
+    def push(self, scope):
+        """Push a new scope to the stack.
+
+        Parameters
+        ----------
+        scope : a Python list
+        """
+        self.stack.append(scope)
+
+    def pop(self):
+        return self.stack.pop()
+
+    def get(self):
+        return self.stack[-1]
+
+    def __repr__(self):
+        return str(self.stack)
+
+    def __len__(self):
+        return len(self.stack)
+
+scope = Scope()
 
 class Operation(object):
     """Base class for all operations.
@@ -31,6 +77,8 @@ class Operation(object):
     def __init__(self, name, loc):
         self.name = name
         self.loc = loc
+        # when an operation is built, its result will be set
+        self.result = None
 
     def __repr__(self):
         return self.name
@@ -47,6 +95,8 @@ class Expr(object):
     def __init__(self, name, loc):
         self.name = name
         self.loc = loc
+        # When an expression is built, its result will be set
+        self.result = None
 
     def __repr__(self):
         return self.name
@@ -71,12 +121,13 @@ class UnaryOp(Expr):
 
     Parameters
     ----------
-    name : str
+    op : str
         The name of the operation
 
     """
-    def __init__(self, name, expr, loc):
-        super().__init__(name, loc)
+    def __init__(self, op, expr, loc):
+        super().__init__(op, loc)
+        expr = immediate_to_constant(expr, loc)
         self.expr = expr
 
     def __repr__(self):
@@ -87,12 +138,14 @@ class BinaryOp(Expr):
 
     Parameters
     ----------
-    name : str
+    op : str
         The name of the operation
 
     """
-    def __init__(self, name, lhs, rhs, loc):
-        super().__init__(name, loc)
+    def __init__(self, op, lhs, rhs, loc):
+        super().__init__(op, loc)
+        lhs = immediate_to_constant(lhs, loc)
+        rhs = immediate_to_constant(rhs, loc)
         self.lhs = lhs
         self.rhs = rhs
 
@@ -104,12 +157,15 @@ class TernaryOp(Expr):
 
     Parameters
     ----------
-    name : str
+    op : str
         The name of the operation
 
     """
-    def __init__(self, name, cond, lhs, rhs, loc):
-        super().__init__(name, loc)
+    def __init__(self, op, cond, lhs, rhs, loc):
+        super().__init__(op, loc)
+        cond = immediate_to_constant(cond, loc)
+        lhs = immediate_to_constant(lhs, loc)
+        rhs = immediate_to_constant(rhs, loc)
         self.cond = cond
         self.lhs = lhs
         self.rhs = rhs
@@ -117,6 +173,21 @@ class TernaryOp(Expr):
     def __repr__(self):
         return f"({self.cond} ? {self.lhs} : {self.rhs})"
 
+class CastOp(Expr):
+    """Cast an expression to a given type.
+
+    Parameters
+    ----------
+
+    """
+    def __init__(self, expr, dtype, loc):
+        super().__init__(dtype_to_str(dtype), loc)
+        expr = immediate_to_constant(expr, loc)
+        self.expr = expr
+        self.dtype = dtype
+
+    def __repr__(self):
+        return f"({self.name} {self.expr} : {self.dtype})"
 
 class Add(BinaryOp):
     """Addition operation.
@@ -154,16 +225,116 @@ class Cmp(BinaryOp):
     def __init__(self, op, lhs, rhs, loc):
         super().__init__(op, lhs, rhs, loc)
 
-class GetItemOp(Expr):
-    """ GetItem operation.
+class ConstantOp(Expr):
+    """Constant operation.
+    """
+    def __init__(self, value, dtype, loc):
+        super().__init__(str(value), loc)
+        self.value = value
+        self.dtype = dtype
+
+class LoadOp(Expr):
+    """ Load operation.
     """
     def __init__(self, tensor, index, loc):
         super().__init__("getitem", loc)
         self.tensor = tensor
         self.index = index
+        self.dtype = tensor.dtype
 
     def __repr__(self):
         return f"{self.tensor.name}[{self.index}]"
+
+class StoreOp(Operation):
+    """ Store operation.
+    """
+    def __init__(self, tensor, index, value, loc):
+        super().__init__("setitem", loc)
+        self.tensor = tensor
+        self.index = index
+        self.value = value
+        self.level = len(scope)
+
+    def __repr__(self):
+        code_str = ""
+        code_str = print_indent(code_str, self.level)
+        code_str += f"{self.tensor.name}[{self.index}] = {self.value}"
+        return code_str
+
+
+class TensorSlice(Expr):
+    def __init__(self, full_shape, op, dtype, parent, indices, loc, name=None):
+        super().__init__(op)
+        self.op = op
+        self.full_shape = full_shape
+        self.dtype = dtype
+        self.name = name
+        self.parent = parent
+        self.indices = indices
+        # calculate tensor slice shape
+        shape = list()
+        dims = 0
+        for index in indices:
+            if isinstance(index, int):
+                dims += 1
+            elif isinstance(index, slice):
+                step = index.step if index.step is not None else 1
+                dim_size = (index.stop - index.start) / step
+                shape.append(int(dim_size))
+                dims += 1
+            # index is an expr
+            elif isinstance(index, Expr):
+                if not hasattr(index, "dtype"):
+                    raise HCLValueError("{} doesn't have dtype".format(index))
+                if not (isinstance(index.dtype, Int) or isinstance(index.dtype, UInt) or isinstance(index, IterVar)): 
+                    raise HCLValueError("{} is not an integer type or index type".format(index))
+                dims += 1
+        for i, dim in enumerate(self.full_shape):
+            if i < dims:
+                continue
+            shape.append(dim)
+        self.shape = tuple(shape)
+
+    def __getitem__(self, indices):
+        if not isinstance(indices, tuple):
+            indices = (indices,)
+        if len(self.indices + indices) < len(self.full_shape):
+            return TensorSlice(
+                self.full_shape,
+                self.op,
+                self.dtype,
+                self.parent,
+                self.indices + indices,
+                self.name,
+            )
+        elif len(self.indices + indices) == len(self.full_shape):
+            # format indices
+            new_indices = []
+            for index in self.indices + indices:
+                if isinstance(index, int):
+                    index = ConstantOp(IndexType.get(), index)
+                new_indices.append(index)
+            load = LoadOp(self.parent, new_indices, self.loc)
+            return load
+        else:
+            raise TensorError("Indices length > # of array dimensions")
+
+    def __setitem__(self, indices, expr):
+        if not isinstance(indices, tuple):
+            indices = (indices,)
+        if len(self.indices + indices) < len(self.full_shape):
+            raise HCLNotImplementedError(
+                "Writing to a slice of tensor is not allowed.")
+        elif len(self.indices + indices) == len(self.full_shape):
+            new_indices = []
+            for index in indices:
+                if isinstance(index, int):
+                    index = ConstantOp(IndexType.get(), index)
+                new_indices.append(index)
+            return StoreOp(self.parent, list(self.indices) + new_indices, expr, self.loc)
+        else:
+            raise TensorError("Indices length > # of array dimensions," \
+                + f"indices=[{self.indices + indices}], shape={self.full_shape}")
 
 class AllocOp(Expr):
     """ Allocate memory for a buffer
@@ -183,7 +354,45 @@ class AllocOp(Expr):
         return f"{self.name} = alloc({self.shape}, {self.dtype})"
 
     def __getitem__(self, indices):
-        return GetItemOp(self, indices, self.loc)
+        if not isinstance(indices, tuple):
+            indices = (indices,)
+        # if we are slicing tensor
+        if len(indices) < len(self.shape):
+            return TensorSlice(
+                self.shape, self.op, self.dtype, self, indices, self.loc, self.name
+            )
+        # if we are loading a value from the tensor
+        elif len(indices) == len(self.shape):
+            # format indices
+            new_indices = []
+            for index in indices:
+                index = immediate_to_constant(index, self.loc)
+                new_indices.append(index)
+            load = LoadOp(self, new_indices, self.loc)
+            return load
+        else:
+            raise TensorError("Indices length > # of array dimensions")
+
+    def __setitem__(self, indices, value):
+        if not isinstance(indices, tuple):
+            indices = (indices,)
+        if len(indices) < len(self.shape):
+            raise HCLNotImplementedError(
+                "Writing to a slice of tensor is not allowed.")
+        elif len(indices) == len(self.shape):
+            # format indices
+            new_indices = []
+            for index in indices:
+                index = immediate_to_constant(index, self.loc)
+                new_indices.append(index)
+            expr = immediate_to_constant(value, self.loc)
+            store_op = StoreOp(self, new_indices, expr, self.loc)
+            # StoreOp is an operation
+            # so we need to add it to the current scope
+            region = scope.get()
+            region.append(store_op)
+        else:
+            raise TensorError("Indices length > # of array dimensions")
 
 
 class ComputeOp(Operation):
@@ -192,20 +401,129 @@ class ComputeOp(Operation):
     def __init__(self, name, shape, body, dtype, loc):
         super().__init__(name, loc)
         self.body = body
+        self.shape = shape
+        self.dtype = dtype
+        self.name = name
         self.tensor = AllocOp(name, shape, dtype, loc)
+        self.level = len(scope)
 
     def __repr__(self):
-        return inspect.getsourcelines(self.body)[0][0].strip()
+        code_str = ""
+        code_str = print_indent(code_str, self.level)
+        code_str += inspect.getsourcelines(self.body)[0][0].strip()
+        return code_str
+
 
 class IfOp(Operation):
     def __init__(self, cond, loc):
         super().__init__('if', loc)
         self.cond = cond
         self.body = list()
+        self.level = len(scope)
 
     def __repr__(self):
-        code_str = "if ({}) {{\n".format(self.cond)
+        code_str = ""
+        code_str = print_indent(code_str, self.level)
+        code_str += "if ({}) {{\n".format(self.cond)
         for stmt in self.body:
-            code_str += f"  {stmt}\n"
+            code_str += f"{stmt}\n"
+        code_str = print_indent(code_str, self.level)
         code_str += "}"
         return code_str
+
+class ElseOp(Operation):
+    def __init__(self, loc):
+        super().__init__('else', loc)
+        self.body = list()
+        self.level = len(scope)
+
+    def __repr__(self):
+        code_str = ""
+        code_str = print_indent(code_str, self.level)
+        code_str += "else {\n"
+        for stmt in self.body:
+            code_str += f"{stmt}\n"
+        code_str = print_indent(code_str, self.level)
+        code_str += "}"
+        return code_str
+
+class ElseIfOp(Operation):
+    def __init__(self, cond, loc):
+        super().__init__('elseif', loc)
+        self.cond = cond
+        self.body = list()
+        self.level = len(scope)
+
+    def __repr__(self):
+        code_str = ""
+        code_str = print_indent(code_str, self.level)
+        code_str += "else if ({}) {{\n".format(self.cond)
+        for stmt in self.body:
+            code_str += f"{stmt}\n"
+        code_str = print_indent(code_str, self.level)
+        code_str += "}"
+        return code_str
+
+class IterVar(Expr):
+    """ Iteration variable.
+    """
+    def __init__(self, name, parent_loop, loc):
+        super().__init__(name, loc)
+        self.parent_loop = parent_loop
+        self.level = len(scope)
+
+    def __repr__(self):
+        return self.name
+
+
+class ForOp(Operation):
+    def __init__(self, name, low, high, step, loc):
+        super().__init__('for', loc)
+        self.name = name
+        self.low = low
+        self.high = high
+        self.step = step
+        self.body = list()
+        self.iter_var = IterVar(name, self, loc)
+        self.level = len(scope)
+
+    def __repr__(self):
+        code_str = ""
+        code_str = print_indent(code_str, self.level)
+        code_str += "for ({} = {}; {} < {}; {} += {}) {{\n".format(
+            self.name, self.low, self.name, self.high, self.name, self.step)
+        for stmt in self.body:
+            code_str += f"{stmt}\n"
+        code_str = print_indent(code_str, self.level)
+        code_str += "}"
+        return code_str
+
+class FuncOp(Operation):
+    def __init__(self, name, args, body, loc):
+        super().__init__('func', loc)
+        self.name = name
+        self.args = args
+        self.body = body
+        self.level = len(scope)
+        self.body_ip = None
+
+    def __repr__(self):
+        code_str = ""
+        code_str = print_indent(code_str, self.level)
+        code_str += "func {}({}) {{\n".format(self.name, ", ".join([v.name for v in self.args]))
+        for stmt in self.body:
+            code_str += f"{stmt}\n"
+        code_str = print_indent(code_str, self.level)
+        code_str += "}"
+        return code_str
+
+class IR(object):
+    def __init__(self):
+        loc = Location("unknown", 0)
+        self.top_func = FuncOp("top", [], [], loc)
+
+    def add_op(self, op):
+        self.top_func.body.append(op)
+
+    def __repr__(self):
+        return str(self.top_func)
---
+++ python/heterocl/ir/ir_builder.py
@@ -0,0 +1,234 @@
+# ===----------------------------------------------------------------------=== #
+#
+# Copyright 2021-2022 The HCL-MLIR Authors.
+#
+# ===----------------------------------------------------------------------=== #
+
+import inspect
+from typing import List, Callable
+
+from . import intermediate as itmd
+from ..context import *
+from ..tensor import Tensor
+import hcl_mlir
+from ..utils import hcl_dtype_to_mlir
+from .. import types as htypes
+# Import MLIR dialects
+# Naming rule: import dialect as dialect_d 
+from hcl_mlir.dialects import \
+    func as func_d, hcl as hcl_d, \
+    scf as scf_d, memref as memref_d, \
+    affine as affine_d
+from hcl_mlir.exceptions import *
+
+class IRBuilder(object):
+    """IRBuilder class to build MLIR
+    operations from intermediate layer
+    """
+    def __init__(self, intermediate):
+        self._intermediate = intermediate
+        self.module = Module.create(get_location())
+        self.iv = [] # TODO(Niansong): what is this list of iv for?
+
+    def build(self):
+        """Builder entry point
+        
+        Build MLIR module with a top-level function
+        """
+        top_func = self._intermediate.top_func
+        with get_context(), get_location():
+            input_types = []
+            for tensor in top_func.args:
+                ele_type = hcl_dtype_to_mlir(tensor.dtype)
+                memref_type = MemRefType.get(tensor.shape, ele_type)
+                input_types.append(memref_type)
+            ip = InsertionPoint(self.module.body)
+            func = func_d.FuncOp(name=top_func.name, type=FunctionType.get(
+                inputs=input_types, results=[]), ip=ip)
+            func.add_entry_block()
+
+            # Set alloc op's result as function block arg
+            for alloc_op, arg in zip(top_func.args, func.entry_block.arguments):
+                alloc_op.result = arg
+
+            # build build's body op
+            ip = InsertionPoint(func.entry_block)
+            for op in top_func.body:
+                self.build_visitor(op, ip)
+            func_d.ReturnOp([], ip=ip)
+
+
+    def build_visitor(self, op, ip):
+        """Build dispatcher
+        
+        Build MLIR operation from intermediate layer
+        """
+        if isinstance(op, itmd.ComputeOp):
+            self.build_compute(op, ip)
+        elif isinstance(op, itmd.AllocOp):
+            self.build_alloc_op(op, ip)
+        elif isinstance(op, itmd.BinaryOp):
+            self.build_binary_op(op, ip)
+        elif isinstance(op, itmd.LoadOp):
+            self.build_load_op(op, ip)
+        elif isinstance(op, itmd.StoreOp):
+            self.build_store_op(op, ip)
+        elif isinstance(op, itmd.ConstantOp):
+            self.build_constant_op(op, ip)
+        else:
+            raise HCLNotImplementedError(f"{type(op)}'s build visitor is not implemented yet.")
+
+
+    def build_compute(self, op, ip):
+        # TODO(Niansong): use real loop names
+        arg_names = ["i%d" % i for i in range(len(op.shape))]
+        with get_context(), get_location():
+            # build output tensor
+            allocOp = itmd.AllocOp(op.name, op.shape, op.dtype, op.loc)
+            self.build_visitor(allocOp, ip)
+            
+            loops = list()
+            for i, (ub, loop_name) in enumerate(zip(op.shape, arg_names)):
+                loop = hcl_mlir.make_for(
+                    0,
+                    ub,
+                    step=1,
+                    name=loop_name,
+                    stage=(op.name if i == 0 else ""),
+                    ip=ip,
+                )
+                loops.append(loop)
+                ip = InsertionPoint(loop.body.operations[0])
+
+            iter_var = [hcl_mlir.IterVar(loop.induction_variable, name=loop_name)
+                for loop, loop_name in zip(loops, arg_names)]
+            result_expr = op.body(*iter_var)
+            # visit the result expression
+            # here ip is the innermost loop
+            self.build_visitor(result_expr, ip)
+            store_op = itmd.StoreOp(allocOp, iter_var, result_expr, op.loc)
+            self.build_visitor(store_op, ip)
+
+    def build_alloc_op(self, op, ip):
+        loc = Location.file(op.loc.filename, op.loc.lineno, 0)
+        ele_type = hcl_dtype_to_mlir(op.dtype)
+        memref_type = MemRefType.get(op.shape, ele_type)
+        op.result = memref_d.AllocOp(memref_type, [], [], ip=ip, loc=loc).result
+
+    def build_binary_op(self, op, ip):
+        # Step 1: build lhs and rhs
+        self.build_visitor(op.lhs, ip)
+        self.build_visitor(op.rhs, ip)
+
+        # Step 2: cast lhs and rhs to the same type
+
+        # Step 3: build binary op
+        loc = Location.file(op.loc.filename, op.loc.lineno, 0)
+
+        # Step 4: attach necessary attributes
+
+    def build_load_op(self, op : itmd.LoadOp, ip):
+        index_exprs = []
+        flag = True
+        load_op = None
+        for index in op.index:
+            try:
+                affine_expr = self.build_affine_expr(index)
+                index_exprs.append(affine_expr)
+            except:
+                flag = False
+                break
+        if flag:
+            affine_map = AffineMap.get(
+                dim_count=len(op.index), symbol_count=0, exprs=index_exprs
+            )
+            affine_attr = AffineMapAttr.get(affine_map)
+            load_op = affine_d.AffineLoadOp(
+                op.tensor.result,
+                [idx.result for idx in op.index],
+                affine_attr,
+                ip=ip
+            )
+            op.result = load_op.result
+        else:
+            new_indices = []
+            for index in op.index:
+                new_indices.append(index.result)
+            load_op = memref_d.LoadOp(op.tensor.result, new_indices, ip=ip)
+            op.result = load_op.result
+
+        load_op.attributes["from"] = StringAttr.get(op.tensor.name)
+        if isinstance(op.dtype, htypes.UInt):
+            load_op.attributes["unsigned"] = UnitAttr.get()
+
+    def build_store_op(self, op : itmd.StoreOp, ip):
+        index_exprs = []
+        flag = True
+        store_op = None
+        for index in op.index:
+            try:
+                affine_expr = self.build_affine_expr(index)
+                index_exprs.append(affine_expr)
+            except:
+                flag = False
+                break
+        if flag:
+            affine_map = AffineMap.get(
+                dim_count=len(op.index), symbol_count=0, exprs=index_exprs
+            )
+            affine_attr = AffineMapAttr.get(affine_map)
+            store_op = affine_d.AffineStoreOp(
+                op.value.result,
+                op.tensor.result,
+                [idx.result for idx in op.index],
+                affine_attr,
+                ip=ip
+            )
+        else:
+            new_indices = []
+            for index in op.index:
+                new_indices.append(index.result)
+            store_op = memref_d.StoreOp(op.value.result, op.tensor.result, new_indices, ip=ip)
+        # we don't need to set the result of store op
+        # because store op doesn't have a result
+        store_op.attributes["to"] = StringAttr.get(op.tensor.name)
+        if isinstance(op.tensor.dtype, htypes.UInt):
+            store_op.attributes["unsigned"] = UnitAttr.get()        
+
+    def build_constant_op(self, op, ip):
+        pass
+
+
+    def build_affine_expr(self, expr):
+        """Build affine expression.
+        * Should all be binary op
+        * AffineExpr can be automatically simplied
+        """
+        if not isinstance(expr, (hcl_mlir.IterVar, itmd.IterVar, itmd.ConstantOp, itmd.CastOp, itmd.BinaryOp)):
+            raise HCLValueError("Not an affine index!")
+        if isinstance(expr, hcl_mlir.IterVar):
+            if isinstance(expr.op.owner.owner, scf_d.ForOp):
+                raise HCLValueError("Outer loop is not affine!")
+            if expr.op not in self.iv:
+                self.iv.append(expr.op)  # BlockArgument
+                return AffineExpr.get_dim(len(self.iv) - 1)
+            else:
+                return AffineExpr.get_dim(self.iv.index(expr.op))
+        elif isinstance(expr, itmd.ConstantOp):
+            return AffineExpr.get_constant(expr.val)
+        elif isinstance(expr, itmd.CastOp):
+            return self.build_affine_expr(expr.val)
+        lhs = self.build_affine_expr(expr.lhs)
+        rhs = self.build_affine_expr(expr.rhs)
+        if isinstance(expr, itmd.AddOp):
+            return lhs + rhs
+        elif isinstance(expr, itmd.SubOp):
+            return lhs - rhs
+        elif isinstance(expr, itmd.MulOp):
+            return lhs * rhs
+        elif isinstance(expr, itmd.DivOp):
+            return AffineExpr.get_floor_div(lhs, rhs)  # or get_ceil_div
+        elif isinstance(expr, itmd.RemOp):
+            return lhs % rhs
+        else:
+            raise HCLValueError("Not an affine index!")
