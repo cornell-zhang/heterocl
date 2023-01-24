1f6f453b5df71e07f22ce0d30cb0c984889bcec4
Niansong Zhang <nz264@cornell.edu>
2023-01-24 17:50:25 -0500

    Add comment with validate pass example

--- python/heterocl/passes/validate.py
+++ python/heterocl/passes/validate.py
@@ -8,3 +8,16 @@
 
 
 # TODO: check if-elif-else chains are valid
+# The following cases are invalid:
+# 1. ElseifOp without ElseIfOp or IfOp in front of it
+# 2. ElseOp without ElseIfOp or IfOp in front of it
+
+# For example, the following AST is invalid:
+# [
+#  else if (cond1) {
+#     ...
+#  }
+#  if (cond2) {
+#     ...
+#  }
+#  ]
