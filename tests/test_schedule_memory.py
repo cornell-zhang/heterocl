e6e23bb9858fadf6c288c17bbaa15507b3b66b33
Niansong Zhang <nz264@cornell.edu>
2023-01-31 16:59:50 -0500

    [Test] Add test cases for #116

--- tests/test_schedule_memory.py
+++ tests/test_schedule_memory.py
@@ -480,3 +480,87 @@ def test_conv2D_lb_wb_schedule():
     np_B = hcl_B.asnumpy()
 
     assert np.array_equal(np_B, np_C)
+
+
+def test_conv2D_lb_wb_stride_2():
+    hcl.init()
+    A = hcl.placeholder((10, 10))
+    r = hcl.reduce_axis(0, 3)
+    c = hcl.reduce_axis(0, 3)
+    B = hcl.compute((4, 4), lambda y, x: hcl.sum(A[y * 2 + r, x * 2 + c], axis=[r, c]))
+    s = hcl.create_schedule([A, B])
+    LB = s.reuse_at(A, s[B], B.axis[0])
+    WB = s.reuse_at(LB, s[B], B.axis[1])
+    f = hcl.build(s)
+
+    np_A = np.random.randint(0, 10, size=(10, 10))
+    np_B = np.zeros((4, 4), dtype="int")
+    np_C = np.zeros((4, 4), dtype="int")
+
+    for y in range(0, 4):
+        for x in range(0, 4):
+            for r in range(0, 3):
+                for c in range(0, 3):
+                    np_C[y][x] += np_A[y * 2 + r][x * 2 + c]
+
+    hcl_A = hcl.asarray(np_A)
+    hcl_B = hcl.asarray(np_B)
+
+    f(hcl_A, hcl_B)
+    np_B = hcl_B.asnumpy()
+    assert np.array_equal(np_B, np_C)
+
+
+def test_avgpool_nchw():
+    bs = 4
+    ic, oc = 16, 16
+    ih, iw = 8, 8
+    kh, kw = 2, 2
+    stride = 1
+    oh, ow = (ih - kh) // stride + 1, (iw - kw) // stride + 1
+    dtype = hcl.Float()
+    hcl.init(dtype)
+    A = hcl.placeholder((bs, ic, ih, iw))
+
+    def avgpool(A):
+        rh = hcl.reduce_axis(0, kh)
+        rw = hcl.reduce_axis(0, kw)
+        B = hcl.compute(
+            (bs, oc, oh, ow),
+            lambda n, c, h, w: hcl.sum(
+                A[n, c, h * stride + rh, w * stride + rw],
+                axis=[rh, rw],
+                dtype=dtype,
+            )
+            / (kh * kw),
+            name="B",
+            dtype=dtype,
+        )
+        return B
+
+    s = hcl.create_schedule([A], avgpool)
+    B = avgpool.B
+    LB = s.reuse_at(A, s[B], B.axis[2])
+    WB = s.reuse_at(LB, s[B], B.axis[3])
+
+    f = hcl.build(s)
+
+    np_A = np.random.random((bs, ic, ih, iw))
+    np_C = np.zeros((bs, oc, oh, ow), dtype="float")
+
+    for n in range(0, bs):
+        for c in range(0, oc):
+            for y in range(0, oh):
+                for x in range(0, ow):
+                    for rh in range(0, kh):
+                        for rw in range(0, kw):
+                            np_C[n][c][y][x] += (
+                                np_A[n][c][y * stride + rh][x * stride + rw]
+                            ) / (kh * kw)
+
+    hcl_A = hcl.asarray(np_A, dtype=dtype)
+    hcl_C = hcl.asarray(np_C, dtype=dtype)
+
+    f(hcl_A, hcl_C)
+
+    assert np.allclose(np_C, hcl_C.asnumpy())
