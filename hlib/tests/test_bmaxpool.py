8ee9eb5fab3cc987727d2aada007449f930eecd0
Niansong Zhang <nz264@cornell.edu>
2023-02-06 15:42:06 -0500

    [hlib][Test] Add tests for binary maxpool

---
+++ hlib/tests/test_bmaxpool.py
@@ -0,0 +1,74 @@
+import heterocl as hcl
+import hlib.bnn as bnn
+import numpy as np
+
+def test_binary_maxpool_nchw():
+    in_shape = (1, 3, 16, 16)
+    out_shape = (1, 3, 8, 8)
+    
+
+    def test_program(A):
+        res = bnn.max_pool2d_nchw(
+            A, 
+            pooling=[2, 2],
+            stride=[2, 2],
+            padding=[0, 0]
+        )
+        return res
+
+    A = hcl.placeholder(in_shape, "A", dtype=hcl.UInt(1))
+    s = hcl.create_schedule([A], test_program)
+    f = hcl.build(s)
+
+    np_A = np.random.randint(0, 2, size=in_shape).astype(np.uint8)
+    hcl_A = hcl.asarray(np_A, hcl.UInt(1))
+    hcl_res = hcl.asarray(np.zeros(out_shape), hcl.UInt(1))
+    f(hcl_A, hcl_res)
+    np_res = hcl_res.asnumpy()
+
+    # reference implementation
+    golden = np.zeros(out_shape)
+    for i in range(out_shape[0]):
+        for j in range(out_shape[1]):
+            for k in range(out_shape[2]):
+                for l in range(out_shape[3]):
+                    golden[i, j, k, l] = np.max(
+                        np_A[i, j, k * 2 : k * 2 + 2, l * 2 : l * 2 + 2]
+                    )
+    assert np.allclose(np_res, golden)
+
+def test_packed_binary_maxpool_nhwc():
+    in_shape = (1, 16, 16, 3)
+    out_shape = (1, 8, 8, 3)
+    
+
+    def test_program(A):
+        res = bnn.packed_max_pool2d_nhwc(
+            A, 
+            pooling=[2, 2],
+            stride=[2, 2],
+            padding=[0, 0]
+        )
+        return res
+
+    A = hcl.placeholder(in_shape, "A", dtype=hcl.UInt(1))
+    s = hcl.create_schedule([A], test_program)
+    f = hcl.build(s)
+
+    np_A = np.random.randint(0, 2, size=in_shape)
+    hcl_A = hcl.asarray(np_A, hcl.UInt(1))
+    hcl_res = hcl.asarray(np.zeros(out_shape), hcl.UInt(1))
+    f(hcl_A, hcl_res)
+    np_res = hcl_res.asnumpy()
+
+    # reference implementation
+    golden = np.zeros(out_shape)
+    for i in range(out_shape[0]):
+        for j in range(out_shape[1]):
+            for k in range(out_shape[2]):
+                for l in range(out_shape[3]):
+                    golden[i, j, k, l] = np.max(
+                        np_A[i, j * 2 : j * 2 + 2, k * 2 : k * 2 + 2, l]
+                    )
+    # bitpack the result along 2nd dimension
+    assert np.allclose(np_res, golden)
