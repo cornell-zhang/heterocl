9f7212f02dcf49e72fe6a980ace86e9f8b591f4d
Niansong Zhang <nz264@cornell.edu>
2023-02-14 21:54:17 -0500

    [Test] Add test_if_LogicalOr

--- tests/test_logical_ops.py
+++ tests/test_logical_ops.py
@@ -24,3 +24,25 @@ def test_while_with_and():
     hcl_res = hcl.asarray(np.zeros((1,), dtype=np.int32), dtype=hcl.UInt(8))
     f(hcl_res)
     assert hcl_res.asnumpy()[0] == 3
+
+
+def test_if_LogicalOr():
+    hcl.init()
+    def kernel():
+        a = hcl.scalar(0, "a", dtype=hcl.UInt(8))
+        b = hcl.scalar(0, "b", dtype=hcl.UInt(8))
+        res0 = hcl.scalar(0, "res0", dtype=hcl.UInt(32))
+        res1 = hcl.scalar(0, "res1", dtype=hcl.UInt(32))
+        with hcl.if_(hcl.or_(a.v == 0, b.v == 0)): # true
+            res0.v = 1
+        with hcl.if_(hcl.or_(a.v == 0, b.v == 0) != 0): # true
+            res1.v = 2
+        return res0, res1
+
+    s = hcl.create_schedule([], kernel)
+    hcl_res0 = hcl.asarray(np.zeros((1,), dtype=np.int32), dtype=hcl.UInt(32))
+    hcl_res1 = hcl.asarray(np.zeros((1,), dtype=np.int32), dtype=hcl.UInt(32))
+    f = hcl.build(s)
+    f(hcl_res0, hcl_res1)
+    assert hcl_res0.asnumpy()[0] == 1
+    assert hcl_res1.asnumpy()[0] == 2
