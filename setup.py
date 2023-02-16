26bdbd4aefaaf185f769e60816c75b15ff98b436
chhzh123 <hc676@cornell.edu>
2023-02-16 01:41:37 -0500

    [setup] Update setup script

--- setup.py
+++ setup.py
@@ -1,18 +1,45 @@
-from setuptools import setup
+import setuptools
 
-setup(
-    name="heterocl",
-    version="0.5",
-    install_requires=[
-        "numpy",
-        "xmltodict",
-        "tabulate",
-        "pytest",
-        "networkx",
-        "matplotlib",
-        "pandas",
-        "imageio",
-        "psutil",
-        "sympy",
-    ],
-)
+
+def setup():
+    with open("README.md", encoding="utf-8") as fp:
+        long_description = fp.read()
+
+    setuptools.setup(
+        name="heterocl",
+        description="HeteroCL: A Multi-Paradigm Programming Infrastructure for Software-Defined Reconfigurable Computing",
+        version="0.5",
+        author="HeteroCL",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        setup_requires=[],
+        install_requires=[
+            "numpy",
+            "xmltodict",
+            "tabulate",
+            "pytest",
+            "networkx",
+            "matplotlib",
+            "pandas",
+            "imageio",
+            "psutil",
+            "sympy",
+        ],
+        packages=setuptools.find_packages(),
+        url="https://github.com/cornell-zhang/heterocl",
+        python_requires=">=3.6",
+        classifiers=[
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: 3.9",
+            "Programming Language :: Python :: 3.10",
+            "Topic :: Scientific/Engineering :: Accelerator Design",
+            "Operating System :: OS Independent",
+        ],
+        zip_safe=True,
+    )
+
+
+if __name__ == "__main__":
+    setup()
