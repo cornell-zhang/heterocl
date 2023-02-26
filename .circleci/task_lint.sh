ac910edd2de8ae0a26b3aba30be7954069ad0c2b
Hongzheng Chen <hc676@cornell.edu>
2023-02-26 16:03:16 -0500

    [CI] Fix CI formatting (#485)
    * Update submodule
    
    * Fix CI formatting

--- .circleci/task_lint.sh
+++ .circleci/task_lint.sh
@@ -8,9 +8,11 @@ set -o pipefail
 
 echo "Check license header..."
 python3 scripts/lint/check_license_header.py HEAD~1
+python3 scripts/lint/check_license_header.py origin/main
 
 echo "Check Python formats using black..."
 bash ./scripts/lint/git-black.sh HEAD~1
+bash ./scripts/lint/git-black.sh origin/main
 
 echo "Running pylint on heterocl"
 python3 -m pylint heterocl --rcfile=./scripts/lint/pylintrc
