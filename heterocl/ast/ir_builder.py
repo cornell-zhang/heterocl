c64cfaec49afaf748f0343f89654ea85e98b6715
Niansong Zhang <nz264@cornell.edu>
2023-03-07 02:07:00 -0500

    [Util] Move make_anywidth_numpy_array to utils

--- heterocl/ast/ir_builder.py
+++ heterocl/ast/ir_builder.py
@@ -52,7 +52,7 @@ from hcl_mlir.exceptions import (
 
 from . import ast
 from ..context import get_context, get_location
-from ..utils import hcl_dtype_to_mlir, get_extra_type_hints
+from ..utils import hcl_dtype_to_mlir, get_extra_type_hints, make_anywidth_numpy_array
 from .. import types as htypes
 from . import build_cleaner
 
@@ -1410,61 +1410,9 @@ class IRBuilder:
         dtype = hcl_dtype_to_mlir(op.dtype, signless=True)
         shape = op.values.shape
         if isinstance(op.dtype, (htypes.Int, htypes.UInt)):
-            # The following code has several steps to convert the numpy array to have
-            # the correct data type in order to create an MLIR constant tensor.
-            # Since MLIR-NumPy Python interface only supports byte-addressable data types,
-            # we need to change the data type of the array to have the minimum number of bytes
-            # that can represent the target bitwidth.
-            # e.g., hcl.const_tensor(arr, dtype=hcl.Int(20)) (6*6 array)
-            #       which requires 20 bits (3 bytes) to represent each element
-            # declaration: 6*6*i20
-            # numpy input: 6*6*i64
-            # 1. Decompose the original i32 or i64 array into a structured array of uint8
-            #  -> decompose: 6*6*8*i8
-            if op.dtype.bits == 1:
-                val = op.values
-                array = np.packbits(val, axis=None, bitorder="little")
-                value_attr = DenseElementsAttr.get(array, shape=val.shape, type=dtype)
-            else:
-                # Here we construct a customized NumPy dtype, "f0", "f1", "f2", etc.
-                # are the field names, and the entire data type is `op.values.dtype`.
-                # This can be viewed as a `union` type in C/C++.
-                # Please refer to the documentation for more details:
-                # https://numpy.org/doc/stable/reference/arrays.dtypes.html#specifying-and-constructing-data-types
-                decomposed_np_dtype = np.dtype(
-                    (
-                        op.values.dtype,
-                        {
-                            f"f{i}": (np.uint8, i)
-                            for i in range(op.values.dtype.itemsize)
-                        },
-                    )
-                )
-                val = op.values.view(decomposed_np_dtype)
-                # 2. Compose the uint8 array into a structured array of target bitwidth
-                # This is done by taking the first several bytes of the uint8 array
-                # "u1" means one unsigned byte, and "i1" means one signed byte
-                n_bytes = int(np.ceil(dtype.width / 8))
-                new_dtype = np.dtype(
-                    {
-                        "names": [f"f{i}" for i in range(n_bytes)],
-                        "formats": (["i1"] if isinstance(dtype, htypes.Int) else ["u1"])
-                        + ["u1"] * (n_bytes - 1),
-                        "offsets": list(range(n_bytes)),
-                        "itemize": n_bytes,
-                    }
-                )
-                # -> compose: 6*6*3*i8
-                val = np.stack([val[f"f{i}"] for i in range(n_bytes)], axis=-1)
-                # -> flatten: 108*i8
-                val = val.flatten()
-                # -> view: 36*i24
-                val = val.view(np.dtype(new_dtype))
-                # -> reshape: 6*6*i24
-                val = val.reshape(shape)
-                # Pass in the numpy array to get the MLIR attribute
-                # -> result: 6*6*i20
-                value_attr = DenseElementsAttr.get(val, shape=val.shape, type=dtype)
+            signed = isinstance(op.dtype, htypes.Int)
+            val = make_anywidth_numpy_array(op.values, op.dtype.bits, signed)
+            value_attr = DenseElementsAttr.get(val, shape=val.shape, type=dtype)
         else:
             val = op.values
             value_attr = DenseElementsAttr.get(val)
--- heterocl/utils.py
+++ heterocl/utils.py
@@ -219,3 +219,75 @@ def get_max_value(dtype):
     if isinstance(dtype, UFixed):
         return (1 << dtype.bits) - 1
     raise DTypeError(f"Unrecognized data type: {dtype}")
+
+
+def make_anywidth_numpy_array(val, bitwidth, signed):
+    """
+    Converts a numpy array to any target bitwidth.
+    ----------------
+    Parameters:
+    val: numpy.ndarray
+        numpy array, can be any numpy native bitwidth, e.g. np.int64
+    bitwidth: int
+        target bitwidth e.g. 9, 31, 198
+    signed: True or False
+        whether the values in the array are signed or unsigned
+    ----------------
+    Returns:
+    numpy.ndarray
+        numpy array with the target bitwidth
+    """
+    shape = val.shape
+    # The following code has several steps to convert the numpy array to have
+    # the correct data type in order to create an MLIR constant tensor.
+    # Since MLIR-NumPy Python interface only supports byte-addressable data types,
+    # we need to change the data type of the array to have the minimum number of bytes
+    # that can represent the target bitwidth.
+    # e.g., hcl.const_tensor(arr, dtype=hcl.Int(20)) (6*6 array)
+    #       which requires 20 bits (3 bytes) to represent each element
+    # declaration: 6*6*i20
+    # numpy input: 6*6*i64
+    # 1. Decompose the original i32 or i64 array into a structured array of uint8
+    #  -> decompose: 6*6*8*i8
+    if bitwidth == 1:
+        return np.packbits(val, axis=None, bitorder="little")
+    else:
+        # Here we construct a customized NumPy dtype, "f0", "f1", "f2", etc.
+        # are the field names, and the entire data type is `op.values.dtype`.
+        # This can be viewed as a `union` type in C/C++.
+        # Please refer to the documentation for more details:
+        # https://numpy.org/doc/stable/reference/arrays.dtypes.html#specifying-and-constructing-data-types
+        decomposed_np_dtype = np.dtype(
+            (
+                val.dtype,
+                {
+                    f"f{i}": (np.uint8, i)
+                    for i in range(val.dtype.itemsize)
+                },
+            )
+        )
+        val = val.view(decomposed_np_dtype)
+        # 2. Compose the uint8 array into a structured array of target bitwidth
+        # This is done by taking the first several bytes of the uint8 array
+        # "u1" means one unsigned byte, and "i1" means one signed byte
+        n_bytes = int(np.ceil(bitwidth / 8))
+        new_dtype = np.dtype(
+            {
+                "names": [f"f{i}" for i in range(n_bytes)],
+                "formats": (["i1"] if signed else ["u1"])
+                + ["u1"] * (n_bytes - 1),
+                "offsets": list(range(n_bytes)),
+                "itemize": n_bytes,
+            }
+        )
+        # -> compose: 6*6*3*i8
+        val = np.stack([val[f"f{i}"] for i in range(n_bytes)], axis=-1)
+        # -> flatten: 108*i8
+        val = val.flatten()
+        # -> view: 36*i24
+        val = val.view(np.dtype(new_dtype))
+        # -> reshape: 6*6*i24
+        val = val.reshape(shape)
+        # Pass in the numpy array to get the MLIR attribute
+        # -> result: 6*6*i20
+        return val
