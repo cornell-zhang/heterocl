83525e99d70d810e379dcdec8e335a2149f527cd
RayLin10 <rl569@cornell.edu>
2023-04-22 21:33:18 -0400

    [AST] Added Math operators to simplify and test cases for it

--- heterocl/ast/ast.py
+++ heterocl/ast/ast.py
@@ -174,6 +174,37 @@ def simplify(expr):
         if simplify(expr.cond):
             return sp.simplify(simplify(expr.true_value))
         return sp.simplify(simplify(expr.false_value))
+    # if isinstance(expr, MathExpOp):
+    #     expr = unwrap_sp(simplify(expr.expr))
+    #     return sp.exp(expr)
+    # if isinstance(expr, MathPowOp):
+    #     lhs = unwrap_sp(simplify(expr.lhs))
+    #     rhs = unwrap_sp(simplify(expr.rhs))
+    #     return sp.simplify(lhs ** rhs)
+    # if isinstance(expr, MathLogOp):
+    #     expr = unwarp_sp(simplify(expr.expr))
+    #     return sp.log(expr)
+    # if isinstance(expr, MathLog2Op):
+    #     expr = unwarp_sp(simplify(expr.expr))
+    #     return sp.log(expr, 2)
+    # if isinstance(expr, MathLog10Op):
+    #     expr = unwarp_sp(simplify(expr.expr))
+    #     return sp.log(expr, 10)
+    if isinstance(expr, MathSqrtOp):
+        expr = unwrap_sp(simplify(expr.expr))
+        return sp.sqrt(expr)
+    # if isinstance(expr, MathSinOp):
+    #     expr = unwrap_sp(simplify(expr.expr))
+    #     return sp.sin(expr)
+    # if isinstance(expr, MathCosOp):
+    #     expr = unwrap_sp(simplify(expr.expr))
+    #     return sp.cos(expr)
+    # if isinstance(expr, MathTanOp):
+    #     expr = unwrap_sp(simplify(expr.expr))
+    #     return sp.tan(expr)
+    # if isinstance(expr, MathTanhOp):
+    #     expr = unwrap_sp(simplify(expr.expr))
+    #     return sp.tanh(expr)
     else:
         raise HCLError(f"Unsupported expression type: {type(expr)}")
 
